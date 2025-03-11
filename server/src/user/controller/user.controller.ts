61153103b45581e1a11adf97b92783875679fe6a
asn6878 <asn6878@gmail.com>
2025-03-12 04:47:44 +0900

    ✨ feat: 이메일 중복 확인 로직 작성

--- server/src/user/controller/user.controller.ts
+++ server/src/user/controller/user.controller.ts
@@ -1,18 +1,39 @@
 import { ApiTags } from '@nestjs/swagger';
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiResponse } from '../../common/response/common.response';
+import { UserService } from '../service/user.service';
+import { SignupDto } from '../dto/request/signup.dto';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get('/email-check')
+  @HttpCode(HttpStatus.OK)
+  async checkEmailDuplication(@Query('email') email: string) {
+    return ApiResponse.responseWithData(
+      '이메일 중복 조회 요청이 성공적으로 처리되었습니다.',
+      {
+        exists: await this.userService.checkEmailDuplication(email),
+      },
+    );
+  }
+
   @Post('/signup')
   @HttpCode(HttpStatus.ACCEPTED)
-  async signupUser(@Body() signupDto: SingupDto) {
-    await this.userService.signupUser(singupDto);
+  async signupUser(@Body() signupDto: SignupDto) {
+    this.userService.signupUser(signupDto);
     return ApiResponse.responseWithNoContent(
-      '회원가입이 성공적으로 처리되었습니다.',
+      '회원가입이 요청이 성공적으로 처리되었습니다.',
     );
   }
 }
--- server/src/user/service/user.service.ts
+++ server/src/user/service/user.service.ts
@@ -1,11 +1,27 @@
 import { UserRepository } from '../repository/user.repository';
+import { Injectable } from '@nestjs/common';
+import { SignupDto } from '../dto/request/signup.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
+  async checkEmailDuplication(email: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    return !!user;
+  }
+
   async signupUser(signupDto: SignupDto) {
-    // do sign up
+    const { email, password, userName } = signupDto;
+
+    // UUID 생성
+
+    // 레디스에 사용자 정보 저장
+    // 이메일 발송
     // check is user exist
     // check is user confirmed
   }
