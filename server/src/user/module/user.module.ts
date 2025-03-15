df0e374a0847b8ffc0b295604af1103136be3971
asn6878 <asn6878@gmail.com>
2025-03-15 18:48:09 +0900

    🐛 fix: 불필요한 모듈 임포트 제거

--- server/src/user/module/user.module.ts
+++ server/src/user/module/user.module.ts
@@ -3,10 +3,9 @@ import { AdminRepository } from '../../admin/repository/admin.repository';
 import { UserRepository } from '../repository/user.repository';
 import { UserService } from '../service/user.service';
 import { EmailModule } from '../../common/email/email.module';
-import { RedisModule } from '../../common/redis/redis.module';
 
 @Module({
-  imports: [RedisModule, EmailModule],
+  imports: [],
   controllers: [],
   providers: [UserService, AdminRepository, UserRepository],
 })
