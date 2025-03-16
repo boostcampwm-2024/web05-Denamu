7cafeb400d7a26676f6304fc9120b4a7faa6d32f
asn6878 <asn6878@gmail.com>
2025-03-17 05:19:14 +0900

    🧼 clean: 미사용 import 제거

--- server/src/user/module/user.module.ts
+++ server/src/user/module/user.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AdminRepository } from '../../admin/repository/admin.repository';
 import { UserRepository } from '../repository/user.repository';
 import { UserService } from '../service/user.service';
-import { EmailModule } from '../../common/email/email.module';
 import { UserController } from '../controller/user.controller';
 
 @Module({
