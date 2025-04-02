3e85d6b17784e60696d870d8b4cc70f7afe5133d
jungmyunggi <jmk101711@yu.ac.kr>
2025-04-02 12:08:53 +0900

    💄 style: 댓글 수정, 삭제, 더보기 디자인 변경

--- client/src/hooks/common/usePostCardActions.ts
+++ client/src/hooks/common/usePostCardActions.ts
@@ -1,6 +1,5 @@
 import { usePostViewIncrement } from "@/hooks/queries/usePostViewIncrement";
 
-// import { pipe } from "@/utils/pipe";
 import { Post } from "@/types/post";
 
 interface PostWithState {
