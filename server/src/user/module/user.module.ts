893e77d1921bb3b93ab76a975adc7eb0daa28d06
CodeVac513 <xssdsw@naver.com>
2025-04-08 20:39:51 +0900

    ✨ feat: user module provider로 ProviderRepository, OAuthService 추가

--- server/src/user/module/user.module.ts
+++ server/src/user/module/user.module.ts
@@ -4,10 +4,20 @@ import { UserRepository } from '../repository/user.repository';
 import { UserService } from '../service/user.service';
 import { UserController } from '../controller/user.controller';
 import { JwtService } from '@nestjs/jwt';
+import { OAuthController } from '../controller/oauth.controller';
+import { OAuthService } from '../service/oauth.service';
+import { ProviderRepository } from '../repository/provider.repository';
 
 @Module({
   imports: [],
-  controllers: [UserController],
-  providers: [UserService, AdminRepository, UserRepository, JwtService],
+  controllers: [UserController, OAuthController],
+  providers: [
+    UserService,
+    OAuthService,
+    AdminRepository,
+    UserRepository,
+    JwtService,
+    ProviderRepository,
+  ],
 })
 export class UserModule {}
