29ba71056d0dda21107367eba489e47ad25185b7
J_Coder <seok3765@naver.com>
2025-04-16 03:13:40 +0900

    🐛 fix: jwt 전략 추가

--- server/src/common/auth/jwt.module.ts
+++ server/src/common/auth/jwt.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { JwtGuard, RefreshJwtGuard } from '../guard/jwt.guard';
+import { JwtRefreshStrategy, JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [JwtService, JwtGuard, RefreshJwtGuard],
+  providers: [
+    JwtService,
+    JwtGuard,
+    RefreshJwtGuard,
+    JwtStrategy,
+    JwtRefreshStrategy,
+  ],
   exports: [JwtService],
 })
 export class JwtAuthModule {}
