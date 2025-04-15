cc4ebb705002ad39542e7a102f9cd6bf768efe7e
J_Coder <seok3765@naver.com>
2025-04-16 01:40:07 +0900

    ✨ feat: jWT 전략 작성, access + refresh 전략

---
+++ server/src/common/auth/jwt.strategy.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Payload } from '../guard/jwt.guard';
+import { Request } from 'express';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  validate(payload: Payload) {
+    return payload;
+  }
+}
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req.cookies['refresh_token'];
+        },
+      ]),
+      secretOrKey: configService.get('JWT_REFRESH_SECRET'),
+    });
+  }
+
+  validate(payload: Payload) {
+    return payload;
+  }
+}
