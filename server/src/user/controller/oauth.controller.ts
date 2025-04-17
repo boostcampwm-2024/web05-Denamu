a563f494aec46c30fdae8d85e9e129a5a0aa3926
CodeVac513 <xssdsw@naver.com>
2025-04-17 16:26:09 +0900

    📝 docs: oauth api swagger 작성

--- server/src/user/controller/oauth.controller.ts
+++ server/src/user/controller/oauth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Req, Res } from '@nestjs/common';
 import { OAuthService } from '../service/oauth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { Response, Request } from 'express';
+import { ApiGoogleOAuth } from '../api-docs/googleOAuth.api-docs';
+import { ApiOAuthCallback } from '../api-docs/oauthCallback.api-docs';
 
 @ApiTags('OAuth')
 @Controller('oauth')
@@ -9,11 +11,13 @@ export class OAuthController {
   constructor(private readonly oauthService: OAuthService) {}
 
   @Get('google')
+  @ApiGoogleOAuth()
   async signupGoogle(@Res() res: Response) {
     return res.redirect(this.oauthService.getGoogleAuthUrl());
   }
 
   @Get('callback')
+  @ApiOAuthCallback()
   async callback(@Req() req: Request, @Res() res: Response) {
     return res.redirect(await this.oauthService.callback(req));
   }
