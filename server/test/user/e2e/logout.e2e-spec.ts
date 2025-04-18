a0a4d43e9146c5b1f9f8a2ebb66c40204c1ffd64
J_Coder <seok3765@naver.com>
2025-04-19 01:20:17 +0900

    ✅ test: 로그아웃 테스트 코드 작성

---
+++ server/test/user/e2e/logout.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { UserService } from '../../../src/user/service/user.service';
+import { UserRepository } from '../../../src/user/repository/user.repository';
+import { UserFixture } from '../../fixture/user.fixture';
+import { User } from '../../../src/user/entity/user.entity';
+
+describe('POST /api/user/logout E2E Test', () => {
+  let app: INestApplication;
+  let userService: UserService;
+  let userInformation: User;
+
+  beforeAll(async () => {
+    app = global.testApp;
+    userService = app.get(UserService);
+    const userRepository = app.get(UserRepository);
+
+    userInformation = await userRepository.save(
+      await UserFixture.createUserCryptFixture(),
+    );
+  });
+
+  it('Access Token이 존재하지 않았을 때, 오류가 발생한다.', async () => {
+    // given
+    const agent = request.agent(app.getHttpServer());
+
+    // when
+    const response = await agent.post('/api/user/logout');
+
+    // then
+    expect(response.status).toBe(401);
+  });
+
+  it('로그아웃을 정상적으로 성공한다.', async () => {
+    // given
+    const accessToken = userService.createAccessToken({
+      id: String(userInformation.id),
+      email: userInformation.email,
+      userName: userInformation.userName,
+      role: 'user',
+    });
+    const agent = request.agent(app.getHttpServer());
+
+    // when
+    const response = await agent
+      .post('/api/user/logout')
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    // then
+    expect(response.status).toBe(200);
+  });
+});
