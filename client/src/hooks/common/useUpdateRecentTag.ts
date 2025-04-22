145a2f847e88681b0ace9ddbcd1da248c9526dbe
정명기 <jmk101711@yu.ac.kr>
2025-04-23 01:06:46 +0900

    ✨ feat: 추천 기능을 위한 hook 추가

---
+++ client/src/hooks/common/useUpdateRecentTag.ts
@@ -0,0 +1,11 @@
+export const useUpdateRecentTags = (newTags: string[]) => {
+  const recentTag = localStorage.getItem("recent-tag");
+  let tags: string[] = recentTag ? JSON.parse(recentTag) : [];
+
+  for (const tag of newTags) {
+    tags = [tag, ...tags.filter((t) => t !== tag)];
+    if (tags.length > 3) tags.pop();
+  }
+
+  localStorage.setItem("recent-tag", JSON.stringify(tags));
+};
