5a30290acf2a0664f08b5c610ebec93dfa681cf9
정명기 <jmk101711@yu.ac.kr>
2025-04-24 23:10:55 +0900

    🐛 fix: 태그 카테고리에 포함되지 않은 태그가 로컬스토리지에 삽입되지 않도록 수정

--- client/src/hooks/common/useUpdateRecentTag.ts
+++ client/src/hooks/common/useUpdateRecentTag.ts
@@ -1,8 +1,13 @@
+import { CATEGORIES } from "@/constants/filter";
+
 export const useUpdateRecentTags = (newTags: string[]) => {
   const recentTag = localStorage.getItem("recent-tag");
   let tags: string[] = recentTag ? JSON.parse(recentTag) : [];
 
   for (const tag of newTags) {
+    const isValid = CATEGORIES.some((category) => category.includes(tag));
+    if (!isValid) continue;
+
     tags = [tag, ...tags.filter((t) => t !== tag)];
     if (tags.length > 3) tags.pop();
   }
