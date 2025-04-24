194bfff64fc7e93279ee919ac2dea695ebde54ed
asn6878 <asn6878@gmail.com>
2025-04-24 17:04:21 +0900

    ♻️ refactor: 태그 삽입 부분 수정

--- feed-crawler/src/repository/tag-map.repository.ts
+++ feed-crawler/src/repository/tag-map.repository.ts
@@ -10,9 +10,17 @@ export class TagMapRepository {
   ) {}
 
   public async insertTags(feedId: number, tags: string[]) {
-    const insertPromises = tags.map((tagName) => {
+    if (!tags?.length) {
+      return;
+    }
+
+    const placeholders = tags.map(() => '?').join(',');
+    const select_tag_query = `SELECT id FROM tag WHERE name IN (${placeholders})`;
+    const tag_ids = await this.dbConnection.executeQuery(select_tag_query, tags)
+
+    const insertPromises = tag_ids.map((tagId) => {
       const query = 'INSERT INTO tag_map (feed_id, tag) VALUES (?, ?)';
-      return this.dbConnection.executeQuery(query, [feedId, tagName]);
+      return this.dbConnection.executeQuery(query, [feedId, tagId]);
     });
 
     await Promise.all(insertPromises);
