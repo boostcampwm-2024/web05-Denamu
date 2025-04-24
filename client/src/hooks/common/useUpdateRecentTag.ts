a749b64501b6b5a1c6c127e2d3ce3ceed0175062
정명기 <jmk101711@yu.ac.kr>
2025-04-24 23:03:15 +0900

    🐛 fix: 로컬 스토리지에 Next.JS가 삽입되지 않도록 수정

--- client/src/hooks/common/useUpdateRecentTag.ts
+++ client/src/hooks/common/useUpdateRecentTag.ts
@@ -3,6 +3,7 @@ export const useUpdateRecentTags = (newTags: string[]) => {
   let tags: string[] = recentTag ? JSON.parse(recentTag) : [];
 
   for (const tag of newTags) {
+    if (tag === "Next.JS") continue;
     tags = [tag, ...tags.filter((t) => t !== tag)];
     if (tags.length > 3) tags.pop();
   }
