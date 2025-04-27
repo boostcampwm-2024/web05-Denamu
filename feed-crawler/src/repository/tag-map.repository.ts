876e5c47d2079b049d809122dc2acd1cb774afe6
DinoDeveloper <asn6878@gmail.com>
2025-04-27 16:33:47 +0900

    ♻️ refactor: executeQuery 시 id 활용하도록 변경

--- feed-crawler/src/repository/tag-map.repository.ts
+++ feed-crawler/src/repository/tag-map.repository.ts
@@ -16,10 +16,16 @@ export class TagMapRepository {
 
     const placeholders = tags.map(() => '?').join(',');
     const select_tag_query = `SELECT id FROM tag WHERE name IN (${placeholders})`;
-    const tag_ids = await this.dbConnection.executeQuery(select_tag_query, tags)
+    const tag_rows = await this.dbConnection.executeQuery<{ id: number }>(
+      select_tag_query,
+      tags,
+    );
+    const tag_ids = tag_rows.map((tag) => tag.id);
 
     if (tags.length !== tag_ids.length) {
-      throw new Error(`${feedId}번 피드 AI 요약 결과에 존재하지 않는 태그 가 포함되어 있습니다.`)
+      throw new Error(
+        `${feedId}번 피드 AI 요약 결과에 존재하지 않는 태그 가 포함되어 있습니다.`,
+      );
     }
 
     const insertPromises = tag_ids.map((tagId) => {
