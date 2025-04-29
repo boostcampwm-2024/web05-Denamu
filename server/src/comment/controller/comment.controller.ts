68744220f204ad31420e3bb0d4bf8e1dae9ce59d
J_Coder <seok3765@naver.com>
2025-04-30 01:33:56 +0900

    ✨ feat: 댓글 등록, 수정, 삭제 API 구현

---
+++ server/src/comment/controller/comment.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CommentService } from '../service/comment.service';
+import { ApiWriteComment } from '../api-docs/writeComment.api-docs';
+import { ApiRemoveComment } from '../api-docs/removeComment.api-docs';
+import { ApiEditComment } from '../api-docs/editComment.api-docs';
+import { JwtGuard } from '../../common/guard/jwt.guard';
+import { ApiResponse } from '../../common/response/common.response';
+import { WriteCommentRequestDto } from '../dto/request/write-comment.dto';
+import { RemoveCommentRequestDto } from '../dto/request/remove-comment.dto';
+import { EditCommentRequestDto } from '../dto/request/edit-comment.dto';
+
+@ApiTags('Comment')
+@Controller('comment')
+@UseGuards(JwtGuard)
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @ApiWriteComment()
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async writeComment(@Req() req, @Body() commentDto: WriteCommentRequestDto) {
+    const userInformation = req.user;
+    await this.commentService.create(userInformation, commentDto);
+    return ApiResponse.responseWithNoContent('댓글 등록을 성공했습니다.');
+  }
+
+  @ApiRemoveComment()
+  @Delete()
+  @HttpCode(HttpStatus.OK)
+  async removeComment(@Req() req, @Body() commentDto: RemoveCommentRequestDto) {
+    const userInformation = req.user;
+    await this.commentService.commentCheck(
+      userInformation,
+      commentDto.commentId,
+    );
+    await this.commentService.remove(commentDto);
+    return ApiResponse.responseWithNoContent('댓글 삭제를 성공했습니다.');
+  }
+
+  @ApiEditComment()
+  @Patch()
+  @HttpCode(HttpStatus.OK)
+  async editComment(@Req() req, @Body() commentDto: EditCommentRequestDto) {
+    const userInformation = req.user;
+    await this.commentService.commentCheck(
+      userInformation,
+      commentDto.commentId,
+    );
+    await this.commentService.edit(commentDto);
+    return ApiResponse.responseWithNoContent('댓글 수정을 성공했습니다.');
+  }
+}
---
+++ server/src/comment/service/comment.service.ts
@@ -0,0 +1,70 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CommentRepository } from '../repository/comment.repository';
+import { WriteCommentRequestDto } from '../dto/request/write-comment.dto';
+import { FeedRepository } from '../../feed/repository/feed.repository';
+import { UserRepository } from '../../user/repository/user.repository';
+import { Payload } from '../../common/guard/jwt.guard';
+import { RemoveCommentRequestDto } from '../dto/request/remove-comment.dto';
+import { EditCommentRequestDto } from '../dto/request/edit-comment.dto';
+
+@Injectable()
+export class CommentService {
+  constructor(
+    private readonly commentRepository: CommentRepository,
+    private readonly feedRepository: FeedRepository,
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async commentCheck(userInformation: Payload, commentId: number) {
+    const commentAuthor = await this.commentRepository.findOneBy({
+      id: commentId,
+    });
+    if (parseInt(userInformation.id) !== commentAuthor.user.id) {
+      throw new UnauthorizedException('본인이 작성한 댓글이 아닙니다.');
+    }
+  }
+
+  async create(userInformation: Payload, commentDto: WriteCommentRequestDto) {
+    const feed = await this.feedRepository.findOneBy({ id: commentDto.feedId });
+    if (!feed) {
+      throw new NotFoundException('존재하지 않는 게시글입니다.');
+    }
+
+    const user = await this.userRepository.findOneBy({
+      id: parseInt(userInformation.id),
+    });
+    if (!user) {
+      throw new NotFoundException('존재하지 않는 유저입니다.');
+    }
+
+    await this.commentRepository.save({
+      comment: commentDto.comment,
+      date: Date.now(),
+      feed: { id: commentDto.feedId },
+      user: { id: user.id },
+    });
+  }
+
+  async remove(commentDto: RemoveCommentRequestDto) {
+    await this.feedRepository.delete({
+      id: commentDto.commentId,
+    });
+  }
+
+  async edit(commentDto: EditCommentRequestDto) {
+    const commentObj = await this.commentRepository.findOneBy({
+      id: commentDto.commentId,
+    });
+
+    if (!commentObj) {
+      throw new NotFoundException('존재하지 않는 댓글입니다.');
+    }
+
+    commentObj.comment = commentDto.newComment;
+    await this.commentRepository.save(commentObj);
+  }
+}
