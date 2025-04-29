4c4bba292fac6cc54cce6c32d45dc45926503f12
J_Coder <seok3765@naver.com>
2025-04-30 01:33:18 +0900

    ✨ feat: comment 엔티티, 레포지토리 기능 구현

---
+++ server/src/comment/entity/comment.entity.ts
@@ -0,0 +1,49 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Feed } from '../../feed/entity/feed.entity';
+import { User } from '../../user/entity/user.entity';
+
+@Entity({ name: 'comment' })
+export class Comment extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: 'comment',
+    nullable: false,
+    type: 'text',
+  })
+  comment: string;
+
+  @Column({
+    name: '',
+    nullable: false,
+  })
+  date: Date;
+
+  @ManyToOne(() => Feed, (feed) => feed.id, {
+    nullable: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({
+    name: 'feed_id',
+  })
+  feed: Feed;
+
+  @ManyToOne(() => User, (user) => user.id, {
+    nullable: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({
+    name: 'user_id',
+  })
+  user: User;
+}
