bc1f02333fc4a6548723e6c4fac405051c16803c
junyeokk <cjh4302@gmail.com>
2025-05-07 12:12:35 +0900

    ♻️ refactor: route 코드 분리

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,98 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route, Location } from "react-router-dom";
+
+import PostDetail from "@/components/common/Card/PostDetail";
+
+import Loading from "@/pages/Loading";
+import PostDetailPage from "@/pages/PostDetailPage";
+import Profile from "@/pages/Profile";
+import SignIn from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
+import UserCertificate from "@/pages/UserCertificate";
+
+const Home = lazy(() => import("@/pages/Home"));
+const Admin = lazy(() => import("@/pages/Admin"));
+const AboutService = lazy(() => import("@/pages/AboutService"));
+
+interface RouterProps {
+  location: Location;
+  state: { backgroundLocation: Location } | null;
+}
+
+export const AppRouter = ({ location, state }: RouterProps) => {
+  return (
+    <>
+      <Routes location={state?.backgroundLocation || location}>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Admin />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Suspense fallback={<Loading />}>
+              <AboutService />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <Suspense fallback={<Loading />}>
+              <SignIn />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Suspense fallback={<Loading />}>
+              <SignUp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/user/certificate"
+          element={
+            <Suspense fallback={<Loading />}>
+              <UserCertificate />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Profile />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/:id"
+          element={
+            <Suspense fallback={<Loading />}>
+              <PostDetailPage />
+            </Suspense>
+          }
+        />
+      </Routes>
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route path="/:id" element={<PostDetail />} />
+        </Routes>
+      )}
+    </>
+  );
+};
