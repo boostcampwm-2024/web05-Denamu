9802d4358b9c04fbcae60e27ccc68da08f7a8c51
J_Coder <seok3765@naver.com>
2025-05-12 02:47:42 +0900

    ✨ feat: metric 모듈 구현

---
+++ server/src/common/metrics/metrics.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import {
+  makeCounterProvider,
+  makeGaugeProvider,
+  makeHistogramProvider,
+  makeSummaryProvider,
+  PrometheusModule,
+} from '@willsoto/nestjs-prometheus';
+
+@Module({
+  imports: [
+    PrometheusModule.register({
+      path: '/metrics',
+      defaultMetrics: {
+        enabled: true,
+      },
+    }),
+  ],
+  providers: [
+    makeCounterProvider({
+      name: 'http_requests_total',
+      help: 'API 요청 전체 수',
+    }),
+    makeCounterProvider({
+      name: 'http_requests_fail',
+      help: 'API 요청 실패 수',
+    }),
+    makeCounterProvider({
+      name: 'http_requests_success',
+      help: 'API 요청 성공 수',
+    }),
+    makeCounterProvider({
+      name: 'anonymous_chat_message_count',
+      help: '익명 채팅 수',
+    }),
+    makeGaugeProvider({
+      name: 'anonymous_chat_user_count',
+      help: '익명 채팅방 현재 접속 인원 수',
+    }),
+    makeHistogramProvider({
+      name: 'http_request_duration_seconds',
+      help: 'API 요청 성공 응답 시간',
+      buckets: [0.1, 0.3, 1.5, 5, 10],
+    }),
+  ],
+})
+export class MetricsModule {}
