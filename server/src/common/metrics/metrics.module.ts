2be0b2c2cc301ba2784421c8477504076e639cbc
J_Coder <seok3765@naver.com>
2025-05-12 03:42:28 +0900

    ♻️ refactor: 모듈에서 exports 추가

--- server/src/common/metrics/metrics.module.ts
+++ server/src/common/metrics/metrics.module.ts
@@ -1,12 +1,49 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import {
   makeCounterProvider,
   makeGaugeProvider,
   makeHistogramProvider,
-  makeSummaryProvider,
   PrometheusModule,
 } from '@willsoto/nestjs-prometheus';
 
+const httpRequestsTotalProvider = makeCounterProvider({
+  name: 'http_requests_total',
+  help: 'API 요청 전체 수',
+  labelNames: ['method', 'route'],
+});
+
+const httpRequestsFailProvider = makeCounterProvider({
+  name: 'http_requests_fail',
+  help: 'API 요청 실패 수',
+  labelNames: ['method', 'route'],
+});
+
+const httpRequestsSuccessProvider = makeCounterProvider({
+  name: 'http_requests_success',
+  help: 'API 요청 성공 수',
+  labelNames: ['method', 'route'],
+});
+
+const anonymousChatMessageCountProvider = makeCounterProvider({
+  name: 'anonymous_chat_message_count',
+  help: '익명 채팅 수',
+  labelNames: ['room'],
+});
+
+const anonymousChatUserCountProvider = makeGaugeProvider({
+  name: 'anonymous_chat_user_count',
+  help: '익명 채팅방 현재 접속 인원 수',
+  labelNames: ['room'],
+});
+
+const httpRequestDurationSecondsProvider = makeHistogramProvider({
+  name: 'http_request_duration_seconds',
+  help: 'API 요청 성공 응답 시간',
+  buckets: [0.1, 0.3, 1.5, 5, 10],
+  labelNames: ['method', 'route'],
+});
+
+@Global()
 @Module({
   imports: [
     PrometheusModule.register({
@@ -17,31 +54,20 @@ import {
     }),
   ],
   providers: [
-    makeCounterProvider({
-      name: 'http_requests_total',
-      help: 'API 요청 전체 수',
-    }),
-    makeCounterProvider({
-      name: 'http_requests_fail',
-      help: 'API 요청 실패 수',
-    }),
-    makeCounterProvider({
-      name: 'http_requests_success',
-      help: 'API 요청 성공 수',
-    }),
-    makeCounterProvider({
-      name: 'anonymous_chat_message_count',
-      help: '익명 채팅 수',
-    }),
-    makeGaugeProvider({
-      name: 'anonymous_chat_user_count',
-      help: '익명 채팅방 현재 접속 인원 수',
-    }),
-    makeHistogramProvider({
-      name: 'http_request_duration_seconds',
-      help: 'API 요청 성공 응답 시간',
-      buckets: [0.1, 0.3, 1.5, 5, 10],
-    }),
+    httpRequestsTotalProvider,
+    httpRequestsFailProvider,
+    httpRequestsSuccessProvider,
+    anonymousChatMessageCountProvider,
+    anonymousChatUserCountProvider,
+    httpRequestDurationSecondsProvider,
+  ],
+  exports: [
+    httpRequestsTotalProvider,
+    httpRequestsFailProvider,
+    httpRequestsSuccessProvider,
+    anonymousChatMessageCountProvider,
+    anonymousChatUserCountProvider,
+    httpRequestDurationSecondsProvider,
   ],
 })
 export class MetricsModule {}
