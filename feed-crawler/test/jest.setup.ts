53099add1fb0a96935656ee49a77ed3bf6845ef5
J_Coder <seok3765@naver.com>
2025-05-16 00:08:36 +0900

    ✅ test: 테스트 sqlite 인메모리 DB -\> mysql 테스트 컨테이너로 변경

---
+++ feed-crawler/test/jest.setup.ts
@@ -0,0 +1,15 @@
+import { setupTestContainer } from './setup/testContext.setup';
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+});
+
+afterEach(async () => {
+  const testContext = setupTestContainer();
+  await testContext.dbConnection.executeQuery('SET FOREIGN_KEY_CHECKS = 0', []);
+  await testContext.dbConnection.executeQuery('TRUNCATE TABLE tag_map', []);
+  await testContext.dbConnection.executeQuery('TRUNCATE TABLE tag', []);
+  await testContext.dbConnection.executeQuery('TRUNCATE TABLE feed', []);
+  await testContext.dbConnection.executeQuery('TRUNCATE TABLE rss_accept', []);
+  await testContext.dbConnection.executeQuery('SET FOREIGN_KEY_CHECKS = 1', []);
+});
