18bd3ea3c85a6984434511df95215d0e71605b7f
DinoDeveloper <asn6878@gmail.com>
2025-05-21 18:24:02 +0900

    ✨ feat: 사용자 활동 업데이트 쿼리 작성 및 서비스 연결

--- server/src/activity/repository/activity.repository.ts
+++ server/src/activity/repository/activity.repository.ts
@@ -1,10 +1,24 @@
 import { DataSource, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { Activity } from '../entity/activity.entity';
+import { User } from '../../user/entity/user.entity';
 
 @Injectable()
 export class ActivityRepository extends Repository<Activity> {
   constructor(private dataSource: DataSource) {
     super(Activity, dataSource.createEntityManager());
+  }
+
+  async upsertByUser(user: User): Promise<void> {
+    await this.createQueryBuilder()
+      .insert()
+      .into(Activity)
+      .values({
+        user: { id: user.id },
+        activityDate: () => 'CURDATE()',
+        viewCount: 1,
+      })
+      .orUpdate(['view_count = view_count + 1'], ['user_id', 'activity_date'])
+      .execute();
   }
 }
--- server/src/activity/service/activity.service.ts
+++ server/src/activity/service/activity.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ActivityRepository } from '../repository/activity.repository';
+import { User } from '../../user/entity/user.entity';
 
 @Injectable()
 export class ActivityService {
@@ -7,8 +8,16 @@ export class ActivityService {
 
   async readActivities(userId: number, year: number) {
     // TODO: 연도별 활동 데이터 전체 조회
-    // + metric 수집
+    // 1. 연도별 활동 데이터 전체 반환 (일, 활동수 쌍)
 
+    // + metric 수집 및 반환 (User Entity에 존재함.)
+    // 1. 최장 읽기 스트릭
+    // 2. 현재 읽기 스트릭
+    // 3. 총 읽은 횟수
     return 'foo';
+  }
+
+  async upsertActivity(user: User) {
+    await this.activityRepository.upsertByUser(user);
   }
 }
