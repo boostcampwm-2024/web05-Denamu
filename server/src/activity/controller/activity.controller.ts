9211aeb6e90ac82f4865ab260830b14b49d202e6
asn6878 <asn6878@gmail.com>
2025-05-27 22:29:06 +0900

    ♻️ refactor: activity 관련 API dto 분리

--- server/src/activity/controller/activity.controller.ts
+++ server/src/activity/controller/activity.controller.ts
@@ -1,19 +1,22 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+
+import { ActivityService } from '../service/activity.service';
+import { ApiResponse } from '../../common/response/common.response';
+import { ActivityParamRequestDto } from '../dto/request/activity-param.dto';
+import { ActivityQueryRequestDto } from '../dto/request/activity-query.dto';
 
 @Controller('activity')
 export class ActivityController {
-  constructor() {}
+  constructor(private readonly activityService: ActivityService) {}
 
-  // 활동 조회 API
   @Get(':userId')
-  async readActivities(@Param userId: number, @Query year: number){
+  async readActivities(
+    @Param() paramDto: ActivityParamRequestDto,
+    @Query() queryDto: ActivityQueryRequestDto,
+  ) {
     return ApiResponse.responseWithData(
       '요청이 성공적으로 처리되었습니다.',
-      await this activityService.readActivities(userId, year);
-    )
+      await this.activityService.readActivities(paramDto.userId, queryDto.year),
+    );
   }
-
-
-
 }
