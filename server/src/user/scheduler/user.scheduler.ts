f92c2e82a18d58d424673dbfd2a7967c081c8a74
DinoDeveloper <asn6878@gmail.com>
2025-05-28 18:26:59 +0900

    ✨ feat: 사용자 활동정보 업데이트 스케줄러 작성성

---
+++ server/src/user/scheduler/user.scheduler.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { UserRepository } from '../repository/user.repository';
+import { Not } from 'typeorm';
+import { WinstonLoggerService } from '../../common/logger/logger.service';
+
+@Injectable()
+export class UserScheduler {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly logger: WinstonLoggerService,
+  ) {}
+
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async resetExpiredStreaks() {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday.setHours(0, 0, 0, 0);
+
+    try {
+      const expiredUsers = await this.userRepository.find({
+        where: {
+          currentStreak: Not(0),
+          lastActiveDate: Not(null),
+        },
+      });
+
+      const usersToUpdate = expiredUsers.filter((user) => {
+        if (!user.lastActiveDate) return false;
+
+        const lastActive = new Date(user.lastActiveDate);
+        lastActive.setHours(0, 0, 0, 0);
+
+        return lastActive < yesterday;
+      });
+
+      if (usersToUpdate.length > 0) {
+        await this.userRepository
+          .createQueryBuilder()
+          .update()
+          .set({ currentStreak: 0 })
+          .whereInIds(usersToUpdate.map((user) => user.id))
+          .execute();
+
+        this.logger.log(
+          `[UserScheduler]: ${usersToUpdate.length} 명의 streak 정보 업데이트 완료.`,
+        );
+      } else {
+        this.logger.log('[UserScheduler]: streak 업데이트 된 사용자 없음.');
+      }
+    } catch (error) {
+      this.logger.error(
+        `[UserScheduler]: streak 업데이트 스케줄러 동작중 오류 발생: ${error.message}`,
+        error.stack,
+      );
+    }
+  }
+}
