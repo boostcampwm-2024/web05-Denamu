6adcfd37ae5243de9d667b8cc15792fb02fb1d43
asn6878 <asn6878@gmail.com>
2025-06-11 02:47:09 +0900

    ✨ feat: 활동 내역 조회 API 구현

--- server/src/activity/module/activity.module.ts
+++ server/src/activity/module/activity.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ActivityRepository } from '../repository/activity.repository';
 import { ActivityController } from '../controller/activity.controller';
 import { ActivityService } from '../service/activity.service';
+import { UserRepository } from '../../user/repository/user.repository';
 
 @Module({
   controllers: [ActivityController],
   imports: [],
-  providers: [ActivityRepository, ActivityService],
+  providers: [ActivityRepository, ActivityService, UserRepository],
   exports: [ActivityService],
 })
 export class ActivityModule {}
