18889608614e99fb34868af462889873e82cfb65
J_Coder <seok3765@naver.com>
2025-06-13 17:19:47 +0900

    🐛 fix: jwt guard 문구 변경

--- server/src/common/guard/jwt.guard.ts
+++ server/src/common/guard/jwt.guard.ts
@@ -1,4 +1,8 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
@@ -16,6 +20,13 @@ export class JwtGuard extends AuthGuard('jwt') {
   ): boolean | Promise<boolean> | Observable<boolean> {
     return super.canActivate(context);
   }
+
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      throw new UnauthorizedException('인증되지 않은 요청입니다.');
+    }
+    return user;
+  }
 }
 
 @Injectable()
@@ -24,5 +35,12 @@ export class RefreshJwtGuard extends AuthGuard('jwt-refresh') {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     return super.canActivate(context);
+  }
+
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      throw new UnauthorizedException('인증되지 않은 요청입니다.');
+    }
+    return user;
   }
 }
