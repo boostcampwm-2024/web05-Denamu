ce1985664c63b96d6b99a50eea697b9b402cb12c
DinoDeveloper <asn6878@gmail.com>
2025-06-14 17:47:25 +0900

    ♻️ refactor: 모듈 import 방식 변경

--- server/src/activity/module/activity.module.ts
+++ server/src/activity/module/activity.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ActivityRepository } from '../repository/activity.repository';
 import { ActivityController } from '../controller/activity.controller';
 import { ActivityService } from '../service/activity.service';
-import { UserRepository } from '../../user/repository/user.repository';
+import { UserModule } from '../../user/module/user.module';
 
 @Module({
   controllers: [ActivityController],
-  imports: [],
-  providers: [ActivityRepository, ActivityService, UserRepository],
+  imports: [UserModule],
+  providers: [ActivityRepository, ActivityService],
   exports: [ActivityService],
 })
 export class ActivityModule {}
