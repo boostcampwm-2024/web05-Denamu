f0309cff7598441dfb608ad9b80d4adf64e874f3
J_Coder <seok3765@naver.com>
2025-06-18 21:15:36 +0900

    🧼 clean: jwt optional 삭제

--- server/src/common/guard/jwt.guard.ts
+++ server/src/common/guard/jwt.guard.ts
@@ -44,10 +44,3 @@ export class RefreshJwtGuard extends AuthGuard('jwt-refresh') {
     return user;
   }
 }
-
-@Injectable()
-export class JwtOptionalGuard extends AuthGuard('jwt') {
-  handleRequest(err: any, user: any, info: any) {
-    return user;
-  }
-}
