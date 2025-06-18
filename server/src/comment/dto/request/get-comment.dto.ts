6ea47df006e419b9bd21a2e935284beeb2b177f4
J_Coder <seok3765@naver.com>
2025-06-19 04:32:06 +0900

    ✨ feat: 댓글 조회 API 구현

---
+++ server/src/comment/dto/request/get-comment.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt } from 'class-validator';
+
+export class GetCommentRequestDto {
+  @ApiProperty({
+    example: '게시글 ID',
+    description: '게시글 ID를 입력해주세요',
+  })
+  @IsInt({
+    message: '숫자로 입력해주세요.',
+  })
+  @Type(() => Number)
+  feedId: number;
+}
