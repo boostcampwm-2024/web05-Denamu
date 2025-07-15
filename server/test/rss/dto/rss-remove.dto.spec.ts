ca53aa94ea477119d9aa4534e4cf1edeca04b3cb
J_Coder <seok3765@naver.com>
2025-07-15 20:21:10 +0900

    ✅ test: rss 삭제 요청 DTO 및 E2E 테스트 작성

---
+++ server/test/rss/dto/rss-remove.dto.spec.ts
@@ -0,0 +1,96 @@
+import { validate } from 'class-validator';
+import { RequestDeleteRssDto } from '../../../src/rss/dto/request/rss-remove.dto';
+
+describe('RequestDeleteRssDto Test', () => {
+  let dto: RequestDeleteRssDto;
+
+  beforeEach(() => {
+    dto = new RequestDeleteRssDto({
+      blogUrl: 'https://test.com/rss',
+      userName: 'test',
+      email: 'test@test.com',
+    });
+  });
+
+  it('블로그 주소가 없다.', async () => {
+    // given
+    delete dto.blogUrl;
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    expect(errors[0].constraints).toHaveProperty(
+      'isUrl',
+      '유효한 URL을 입력해주세요.',
+    );
+  });
+
+  it('블로그 주소가 올바르지 않다.', async () => {
+    // given
+    dto.blogUrl = 'just string';
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    expect(errors[0].constraints).toHaveProperty(
+      'isUrl',
+      '유효한 URL을 입력해주세요.',
+    );
+  });
+
+  it('사용자 이름이 없다.', async () => {
+    // given
+    delete dto.userName;
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    expect(errors[0].constraints).toHaveProperty(
+      'isNotEmpty',
+      '유저 이름은 반드시 입력해야 합니다.',
+    );
+  });
+  it('사용자 이름이 문자열이 아니다.', async () => {
+    // given
+    dto.userName = 100 as any;
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    expect(errors[0].constraints).toHaveProperty(
+      'isString',
+      '문자열로 입력해주세요.',
+    );
+  });
+  it('이메일 주소가 없다.', async () => {
+    // given
+    delete dto.email;
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    expect(errors[0].constraints).toHaveProperty(
+      'isEmail',
+      '올바른 이메일 주소를 입력하세요.',
+    );
+  });
+
+  it('이메일 주소가 올바르지 않다.', async () => {
+    // given
+    dto.email = 'just string';
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    expect(errors[0].constraints).toHaveProperty(
+      'isEmail',
+      '올바른 이메일 주소를 입력하세요.',
+    );
+  });
+});
---
+++ server/test/rss/e2e/remove.e2e-spec.ts
@@ -0,0 +1,69 @@
+import { INestApplication } from '@nestjs/common';
+import {
+  RssAcceptRepository,
+  RssRepository,
+} from '../../../src/rss/repository/rss.repository';
+import { RssFixture } from '../../fixture/rss.fixture';
+import * as request from 'supertest';
+
+describe('/api/rss/remove E2E Test', () => {
+  let app: INestApplication;
+  let rssRepository: RssRepository;
+  let rssAcceptRepository: RssAcceptRepository;
+
+  beforeAll(() => {
+    app = global.testApp;
+    rssRepository = app.get(RssRepository);
+    rssAcceptRepository = app.get(RssAcceptRepository);
+  });
+
+  describe('POST /api/rss/remove E2E Test', () => {
+    it('[404] RSS가 없을 경우 신청할 수 없다.', async () => {
+      // given
+      // when
+      const response = await request(app.getHttpServer())
+        .post('/api/rss/remove')
+        .send({
+          blogUrl: 'https://test.com',
+          userName: 'test',
+          email: 'test@test.com',
+        });
+      // then
+      expect(response.status).toBe(404);
+    });
+
+    it('[200] 대기 RSS가 있을 경우 신청할 수 있다.', async () => {
+      // given
+      const rss = await rssRepository.save(RssFixture.createRssFixture());
+
+      // when
+      const response = await request(app.getHttpServer())
+        .post('/api/rss/remove')
+        .send({
+          blogUrl: rss.rssUrl,
+          userName: rss.userName,
+          email: rss.email,
+        });
+
+      // then
+      expect(response.status).toBe(200);
+    });
+
+    it('[200] 승인된 RSS가 있을 경우 신청할 수 있다.', async () => {
+      // given
+      const rss = await rssAcceptRepository.save(RssFixture.createRssFixture());
+
+      // when
+      const response = await request(app.getHttpServer())
+        .post('/api/rss/remove')
+        .send({
+          blogUrl: rss.rssUrl,
+          userName: rss.userName,
+          email: rss.email,
+        });
+
+      // then
+      expect(response.status).toBe(200);
+    });
+  });
+});
