ed94af26aa92c5b9d349d48a49a5233f2bde81e7
J_Coder <seok3765@naver.com>
2025-07-19 23:39:35 +0900

    🧼 clean: 코드 잘못된 이름 및 문자열 변경

--- server/src/migration/1619999999999-CreateActivity.ts
+++ server/src/migration/1619999999999-CreateActivity.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateRssAccept1619999999999 implements MigrationInterface {
+export class CreateActivity1619999999999 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE \`activity\` (
