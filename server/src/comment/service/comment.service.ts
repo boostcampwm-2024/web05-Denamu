c675c6e7f5574b28bf3177da40964473fc88006d
CodeVac513 <xssdsw@naver.com>
2025-08-05 14:01:27 +0900

    ✨ feat: feed의 comment_count와 comment 엔티티의 생성/삭제를 하나의 트랜잭션으로 관리하도록 코드 수정

--- server/src/comment/service/comment.service.ts
+++ server/src/comment/service/comment.service.ts
@@ -11,6 +11,8 @@ import { Payload } from '../../common/guard/jwt.guard';
 import { DeleteCommentRequestDto } from '../dto/request/delete-comment.dto';
 import { UpdateCommentRequestDto } from '../dto/request/update-comment.dto';
 import { GetCommentRequestDto } from '../dto/request/get-comment.dto';
+import { DataSource } from 'typeorm';
+import { Comment } from '../entity/comment.entity';
 
 @Injectable()
 export class CommentService {
@@ -18,6 +20,7 @@ export class CommentService {
     private readonly commentRepository: CommentRepository,
     private readonly feedRepository: FeedRepository,
     private readonly userRepository: UserRepository,
+    private readonly dataSource: DataSource,
   ) {}
 
   private async commentCheck(userInformation: Payload, commentId: number) {
@@ -35,6 +38,8 @@ export class CommentService {
     if (userInformation.id !== commentObj.user.id) {
       throw new UnauthorizedException('본인이 작성한 댓글이 아닙니다.');
     }
+
+    return commentObj;
   }
 
   async get(commentDto: GetCommentRequestDto) {
@@ -62,30 +67,66 @@ export class CommentService {
   }
 
   async create(userInformation: Payload, commentDto: CreateCommentRequestDto) {
-    const feed = await this.feedRepository.findOneBy({ id: commentDto.feedId });
-    if (!feed) {
-      throw new NotFoundException('존재하지 않는 게시글입니다.');
-    }
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    const user = await this.userRepository.findOneBy({
-      id: userInformation.id,
-    });
-    if (!user) {
-      throw new NotFoundException('존재하지 않는 유저입니다.');
-    }
+    try {
+      const feed = await this.feedRepository.findOneBy({
+        id: commentDto.feedId,
+      });
+      if (!feed) {
+        throw new NotFoundException('존재하지 않는 게시글입니다.');
+      }
 
-    await this.commentRepository.save({
-      comment: commentDto.comment,
-      feed,
-      user,
-    });
+      const user = await this.userRepository.findOneBy({
+        id: userInformation.id,
+      });
+
+      if (!user) {
+        throw new NotFoundException('존재하지 않는 유저입니다.');
+      }
+
+      feed.commentCount++;
+      await queryRunner.manager.save(feed);
+      await queryRunner.manager.save(Comment, {
+        comment: commentDto.comment,
+        feed: { id: commentDto.feedId },
+        user: { id: userInformation.id },
+      });
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async delete(userInformation: Payload, commentDto: DeleteCommentRequestDto) {
-    await this.commentCheck(userInformation, commentDto.commentId);
-    await this.feedRepository.delete({
-      id: commentDto.commentId,
-    });
+    const comment = await this.commentCheck(
+      userInformation,
+      commentDto.commentId,
+    );
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const feed = comment.feed;
+      feed.commentCount--;
+      await queryRunner.manager.save(feed);
+      await queryRunner.manager.remove(comment);
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async update(userInformation: Payload, commentDto: UpdateCommentRequestDto) {
