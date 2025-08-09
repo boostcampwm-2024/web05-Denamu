9db455d11a7818feff7cb5092cee11a9ffad5d29
J_Coder <seok3765@naver.com>
2025-08-10 05:42:15 +0900

    ♻️ refactor: response DTO 사용하도록 변경

--- server/src/comment/service/comment.service.ts
+++ server/src/comment/service/comment.service.ts
@@ -6,18 +6,21 @@ import {
 import { CommentRepository } from '../repository/comment.repository';
 import { CreateCommentRequestDto } from '../dto/request/create-comment.dto';
 import { FeedRepository } from '../../feed/repository/feed.repository';
-import { UserRepository } from '../../user/repository/user.repository';
 import { Payload } from '../../common/guard/jwt.guard';
 import { DeleteCommentRequestDto } from '../dto/request/delete-comment.dto';
 import { UpdateCommentRequestDto } from '../dto/request/update-comment.dto';
 import { GetCommentRequestDto } from '../dto/request/get-comment.dto';
+import { GetCommentResponseDto } from '../dto/response/get-comment.dto';
+import { FeedService } from '../../feed/service/feed.service';
+import { UserService } from '../../user/service/user.service';
 
 @Injectable()
 export class CommentService {
   constructor(
     private readonly commentRepository: CommentRepository,
     private readonly feedRepository: FeedRepository,
-    private readonly userRepository: UserRepository,
+    private readonly userService: UserService,
+    private readonly feedService: FeedService,
   ) {}
 
   private async commentCheck(userInformation: Payload, commentId: number) {
@@ -38,41 +41,17 @@ export class CommentService {
   }
 
   async get(commentDto: GetCommentRequestDto) {
-    const feed = await this.feedRepository.findOneBy({
-      id: commentDto.feedId,
-    });
-
-    if (!feed) {
-      throw new NotFoundException('게시글을 찾을 수 없습니다.');
-    }
+    await this.feedService.getFeed(commentDto.feedId);
 
     const comments = await this.commentRepository.getCommentInformation(
       commentDto.feedId,
     );
-    return comments.map((row) => ({
-      id: row.id,
-      comment: row.comment,
-      date: row.date,
-      user: {
-        id: row.user.id,
-        userName: row.user.userName,
-        profileImage: row.user.profileImage,
-      },
-    }));
+    return GetCommentResponseDto.toResponseDtoArray(comments);
   }
 
   async create(userInformation: Payload, commentDto: CreateCommentRequestDto) {
-    const feed = await this.feedRepository.findOneBy({ id: commentDto.feedId });
-    if (!feed) {
-      throw new NotFoundException('존재하지 않는 게시글입니다.');
-    }
-
-    const user = await this.userRepository.findOneBy({
-      id: userInformation.id,
-    });
-    if (!user) {
-      throw new NotFoundException('존재하지 않는 유저입니다.');
-    }
+    const feed = await this.feedService.getFeed(commentDto.feedId);
+    const user = await this.userService.getUser(userInformation.id);
 
     await this.commentRepository.save({
       comment: commentDto.comment,
