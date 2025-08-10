f026b8f6e4d122a272e4f02ae7cee8ec3901a040
codeVac513 <xssdsw@naver.com>
2025-08-10 16:10:22 +0900

    🧼 clean: migration 코드 내 name 속성 삭제

--- server/src/common/database/migration/1754027996037-AlterFeedCommentCountColumn.ts
+++ server/src/common/database/migration/1754027996037-AlterFeedCommentCountColumn.ts
@@ -3,8 +3,6 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AlterFeedCommentCountColumn1754027996037
   implements MigrationInterface
 {
-  name = 'AlterFeedCommentCountColumn1754027996037';
-
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE \`feed\`
       ADD \`comment_count\` int NOT NULL DEFAULT '0'`);
