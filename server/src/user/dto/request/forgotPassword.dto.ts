8c6738bfa1ef0ecf07da4030e464f6cae951096a
codeVac513 <xssdsw@naver.com>
2025-09-17 11:32:07 +0900

    ✨ feat: 비밀번호 변경 요청 API의 DTO 구현

---
+++ server/src/user/dto/request/forgotPassword.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ForgotPasswordRequestDto {
+  @ApiProperty({
+    example: 'example@example.com',
+    description: '이메일을 입력해주세요.',
+  })
+  @IsEmail(
+    {},
+    {
+      message: '이메일 주소 형식에 맞춰서 작성해주세요.',
+    },
+  )
+  @IsNotEmpty({
+    message: '이메일이 없습니다.',
+  })
+  email: string;
+}
