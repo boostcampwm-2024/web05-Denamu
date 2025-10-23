c6cdd6c835415b5f55113a16c8a270c1a48d163a
CodeVac513 <xssdsw@naver.com>
2025-10-23 21:26:07 +0900

    ✨ feat: rabbitmq service 클래스 구현

--- server/src/common/rabbitmq/rabbitmq.service.ts
+++ server/src/common/rabbitmq/rabbitmq.service.ts
@@ -1,10 +1,31 @@
-import { Inject, Injectable } from '@nestjs/common';
-import * as amqp from 'amqplib';
+import { Injectable } from '@nestjs/common';
+import { RabbitMQManager } from './rabbitmq.manager';
+import { ConsumeMessage } from 'amqplib';
 
 @Injectable()
 export class RabbitMQService {
-  constructor(
-    @Inject('RABBITMQ_CONNECTION')
-    public readonly connection: amqp.ChannelModel,
-  ) {}
+  constructor(private readonly rabbitMQManager: RabbitMQManager) {}
+
+  async sendMessage(exchange: string, routingKey: string, message: string) {
+    const channel = await this.rabbitMQManager.getChannel();
+    channel.publish(exchange, routingKey, Buffer.from(message));
+  }
+
+  async consumeMessage(
+    queue: string,
+    onMessage: (msg: ConsumeMessage | null) => void | Promise<void>,
+  ) {
+    const channel = await this.rabbitMQManager.getChannel();
+    await channel.consume(queue, async (message) => {
+      try {
+        const parsedMessage = JSON.parse(message.content.toString());
+
+        await onMessage(message);
+
+        channel.ack(message);
+      } catch (err) {
+        channel.nack(message, false, false);
+      }
+    });
+  }
 }
