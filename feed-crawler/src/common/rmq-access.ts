a8b75b8326d50624e32bd411df70a6c46ef5ad94
CodeVac513 <xssdsw@naver.com>
2025-10-23 21:17:06 +0900

    ✨ feat: 메시지 처리를 위한 클래스 구현

--- feed-crawler/src/common/rmq-access.ts
+++ feed-crawler/src/common/rmq-access.ts
@@ -1,25 +1,39 @@
-import { injectable } from 'tsyringe';
-import * as amqp from 'amqplib';
-import { Channel, ChannelModel } from 'amqplib';
+import { inject, injectable } from 'tsyringe';
+import { RabbitMQManager } from './rabbitmq.manager';
+import { ConsumeMessage } from 'amqplib/properties';
+import { DEPENDENCY_SYMBOLS } from '../types/dependency-symbols';
 
 @injectable()
 export class RabbitMQConnection {
-  private connection: ChannelModel;
-  private channel: Channel;
   private nameTag: string;
 
-  constructor() {
+  constructor(
+    @inject(DEPENDENCY_SYMBOLS.RabbitMQManager)
+    private readonly rabbitMQManager: RabbitMQManager,
+  ) {
     this.nameTag = '[RabbitMQ]';
-    this.connect();
   }
 
-  async connect() {
-    this.connection = await amqp.connect({
-      protocol: 'amqp',
-      hostname: process.env.RABBITMQ_HOST,
-      port: Number.parseInt(process.env.RABBITMQ_PORT),
-      username: process.env.RABBITMQ_USERNAME,
-      password: process.env.RABBITMQ_PASSWORD,
+  async sendMessage(exchange: string, routingKey: string, message: string) {
+    const channel = await this.rabbitMQManager.getChannel();
+    channel.publish(exchange, routingKey, Buffer.from(message));
+  }
+
+  async consumeMessage(
+    queue: string,
+    onMessage: (msg: ConsumeMessage | null) => void | Promise<void>,
+  ) {
+    const channel = await this.rabbitMQManager.getChannel();
+    await channel.consume(queue, async (message) => {
+      try {
+        const parsedMessage = JSON.parse(message.content.toString());
+
+        await onMessage(message);
+
+        channel.ack(message);
+      } catch (err) {
+        channel.nack(message, false, false);
+      }
     });
   }
 }
