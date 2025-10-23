150b555662728ec4adaf46f181f790cbb441f5f0
CodeVac513 <xssdsw@naver.com>
2025-10-23 21:00:42 +0900

    ✨ feat: exchange, queue 생성 및 바인딩을 위한 설정 구현

---
+++ feed-crawler/src/common/rabbitmq.config.ts
@@ -0,0 +1,73 @@
+import { inject, injectable } from 'tsyringe';
+import { RabbitMQManager } from './rabbitmq.manager';
+import {
+  RMQ_EXCHANGES,
+  RMQ_EXCHANGE_TYPE,
+  RMQ_QUEUES,
+  RMQ_ROUTING_KEYS,
+} from './rabbitmq.constant';
+import { DEPENDENCY_SYMBOLS } from '../types/dependency-symbols';
+
+@injectable()
+export class RabbitMQConfig {
+  constructor(
+    @inject(DEPENDENCY_SYMBOLS.RabbitMQManager)
+    private readonly rabbitMQManager: RabbitMQManager,
+  ) {}
+
+  async setup() {
+    const channel = await this.rabbitMQManager.getChannel();
+
+    //exchange 생성
+    await channel.assertExchange(RMQ_EXCHANGES.EMAIL, RMQ_EXCHANGE_TYPE.DIRECT);
+    await channel.assertExchange(
+      RMQ_EXCHANGES.CRAWLING,
+      RMQ_EXCHANGE_TYPE.TOPIC,
+    );
+    await channel.assertExchange(
+      RMQ_EXCHANGES.DEAD_LETTER,
+      RMQ_EXCHANGE_TYPE.TOPIC,
+    );
+
+    // DLQ 생성
+    await channel.assertQueue(RMQ_QUEUES.CRAWLING_FULL_DEAD_LETTER, {
+      durable: true,
+    });
+    await channel.assertQueue(RMQ_QUEUES.EMAIL_DEAD_LETTER, { durable: true });
+
+    //queue 생성
+    await channel.assertQueue(RMQ_QUEUES.EMAIL_SEND, {
+      durable: true,
+      deadLetterExchange: RMQ_EXCHANGES.DEAD_LETTER,
+      deadLetterRoutingKey: RMQ_ROUTING_KEYS.EMAIL_DEAD_LETTER,
+    });
+    await channel.assertQueue(RMQ_QUEUES.CRAWLING_FULL, {
+      durable: true,
+      deadLetterExchange: RMQ_EXCHANGES.DEAD_LETTER,
+      deadLetterRoutingKey: RMQ_ROUTING_KEYS.CRAWLING_FULL_DEAD_LETTER,
+    });
+
+    // DLQ 바인딩
+    await channel.bindQueue(
+      RMQ_QUEUES.EMAIL_DEAD_LETTER,
+      RMQ_EXCHANGES.DEAD_LETTER,
+      RMQ_ROUTING_KEYS.EMAIL_DEAD_LETTER,
+    );
+    await channel.bindQueue(
+      RMQ_QUEUES.CRAWLING_FULL_DEAD_LETTER,
+      RMQ_EXCHANGES.DEAD_LETTER,
+      RMQ_ROUTING_KEYS.CRAWLING_FULL_DEAD_LETTER,
+    );
+    // queue 바인딩
+    await channel.bindQueue(
+      RMQ_QUEUES.EMAIL_SEND,
+      RMQ_EXCHANGES.EMAIL,
+      RMQ_ROUTING_KEYS.EMAIL_SEND,
+    );
+    await channel.bindQueue(
+      RMQ_QUEUES.CRAWLING_FULL,
+      RMQ_EXCHANGES.CRAWLING,
+      RMQ_ROUTING_KEYS.CRAWLING_FULL,
+    );
+  }
+}
