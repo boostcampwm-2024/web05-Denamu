e085f28dd7711e7517da62e77a202bdaa830a463
CodeVac513 <xssdsw@naver.com>
2025-11-12 15:30:09 +0900

    ✨ feat: rabbitmq 설정 시 오류 발생을 대비한 try/catch 구문 추가

--- server/src/common/rabbitmq/rabbitmq.config.ts
+++ server/src/common/rabbitmq/rabbitmq.config.ts
@@ -18,56 +18,66 @@ export class RabbitMQConfig implements OnModuleInit {
   async setup() {
     const channel = await this.rabbitMQManager.getChannel();
 
-    //exchange 생성
-    await channel.assertExchange(RMQ_EXCHANGES.EMAIL, RMQ_EXCHANGE_TYPE.DIRECT);
-    await channel.assertExchange(
-      RMQ_EXCHANGES.CRAWLING,
-      RMQ_EXCHANGE_TYPE.TOPIC,
-    );
-    await channel.assertExchange(
-      RMQ_EXCHANGES.DEAD_LETTER,
-      RMQ_EXCHANGE_TYPE.TOPIC,
-    );
+    try {
+      //exchange 생성
+      await channel.assertExchange(
+        RMQ_EXCHANGES.EMAIL,
+        RMQ_EXCHANGE_TYPE.DIRECT,
+      );
+      await channel.assertExchange(
+        RMQ_EXCHANGES.CRAWLING,
+        RMQ_EXCHANGE_TYPE.TOPIC,
+      );
+      await channel.assertExchange(
+        RMQ_EXCHANGES.DEAD_LETTER,
+        RMQ_EXCHANGE_TYPE.TOPIC,
+      );
 
-    // DLQ 생성
-    await channel.assertQueue(RMQ_QUEUES.CRAWLING_FULL_DEAD_LETTER, {
-      durable: true,
-    });
-    await channel.assertQueue(RMQ_QUEUES.EMAIL_DEAD_LETTER, { durable: true });
+      // DLQ 생성
+      await channel.assertQueue(RMQ_QUEUES.CRAWLING_FULL_DEAD_LETTER, {
+        durable: true,
+      });
+      await channel.assertQueue(RMQ_QUEUES.EMAIL_DEAD_LETTER, {
+        durable: true,
+      });
 
-    //queue 생성
-    await channel.assertQueue(RMQ_QUEUES.EMAIL_SEND, {
-      durable: true,
-      deadLetterExchange: RMQ_EXCHANGES.DEAD_LETTER, // DLX 설정
-      deadLetterRoutingKey: RMQ_ROUTING_KEYS.EMAIL_DEAD_LETTER, //Routing Key 설정을 통해 DLQ 설정
-    });
-    await channel.assertQueue(RMQ_QUEUES.CRAWLING_FULL, {
-      durable: true,
-      deadLetterExchange: RMQ_EXCHANGES.DEAD_LETTER,
-      deadLetterRoutingKey: RMQ_ROUTING_KEYS.CRAWLING_FULL_DEAD_LETTER,
-    });
+      //queue 생성
+      await channel.assertQueue(RMQ_QUEUES.EMAIL_SEND, {
+        durable: true,
+        deadLetterExchange: RMQ_EXCHANGES.DEAD_LETTER,
+        deadLetterRoutingKey: RMQ_ROUTING_KEYS.EMAIL_DEAD_LETTER,
+      });
+      await channel.assertQueue(RMQ_QUEUES.CRAWLING_FULL, {
+        durable: true,
+        deadLetterExchange: RMQ_EXCHANGES.DEAD_LETTER,
+        deadLetterRoutingKey: RMQ_ROUTING_KEYS.CRAWLING_FULL_DEAD_LETTER,
+      });
 
-    // DLQ 바인딩
-    await channel.bindQueue(
-      RMQ_QUEUES.EMAIL_DEAD_LETTER,
-      RMQ_EXCHANGES.DEAD_LETTER,
-      RMQ_ROUTING_KEYS.EMAIL_DEAD_LETTER,
-    );
-    await channel.bindQueue(
-      RMQ_QUEUES.CRAWLING_FULL_DEAD_LETTER,
-      RMQ_EXCHANGES.DEAD_LETTER,
-      RMQ_ROUTING_KEYS.CRAWLING_FULL_DEAD_LETTER,
-    );
-    // queue 바인딩
-    await channel.bindQueue(
-      RMQ_QUEUES.EMAIL_SEND,
-      RMQ_EXCHANGES.EMAIL,
-      RMQ_ROUTING_KEYS.EMAIL_SEND,
-    );
-    await channel.bindQueue(
-      RMQ_QUEUES.CRAWLING_FULL,
-      RMQ_EXCHANGES.CRAWLING,
-      RMQ_ROUTING_KEYS.CRAWLING_FULL,
-    );
+      // DLQ 바인딩
+      await channel.bindQueue(
+        RMQ_QUEUES.EMAIL_DEAD_LETTER,
+        RMQ_EXCHANGES.DEAD_LETTER,
+        RMQ_ROUTING_KEYS.EMAIL_DEAD_LETTER,
+      );
+      await channel.bindQueue(
+        RMQ_QUEUES.CRAWLING_FULL_DEAD_LETTER,
+        RMQ_EXCHANGES.DEAD_LETTER,
+        RMQ_ROUTING_KEYS.CRAWLING_FULL_DEAD_LETTER,
+      );
+
+      // queue 바인딩
+      await channel.bindQueue(
+        RMQ_QUEUES.EMAIL_SEND,
+        RMQ_EXCHANGES.EMAIL,
+        RMQ_ROUTING_KEYS.EMAIL_SEND,
+      );
+      await channel.bindQueue(
+        RMQ_QUEUES.CRAWLING_FULL,
+        RMQ_EXCHANGES.CRAWLING,
+        RMQ_ROUTING_KEYS.CRAWLING_FULL,
+      );
+    } catch (error) {
+      throw new Error(`RabbitMQ 설정 초기화 실패: ${error.message}`);
+    }
   }
 }
