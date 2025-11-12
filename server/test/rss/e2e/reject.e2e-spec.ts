d18fb261fa3bcb04cac43bc80669d4e0ed13a00f
J_Coder <seok3765@naver.com>
2025-11-12 17:10:33 +0900

    ✅ test: describe 범위 통일

--- server/test/rss/e2e/reject.e2e-spec.ts
+++ server/test/rss/e2e/reject.e2e-spec.ts
@@ -32,63 +32,59 @@ describe('POST /api/rss/reject/{rssId} E2E Test', () => {
     ]);
   });
 
-  describe('정상적인 요청을 한다.', () => {
-    it('[201] 정상적으로 RSS를 거절한다.', async () => {
-      // given
-      const REJECT_REASON = '거절 사유';
-      const rss = await rssRepository.save(RssFixture.createRssFixture());
-      const rejectRssDto = new RejectRssRequestDto({
-        description: REJECT_REASON,
-      });
-
-      // when
-      const response = await request(app.getHttpServer())
-        .post(`/api/rss/reject/${rss.id}`)
-        .set('Cookie', 'sessionId=testSessionId')
-        .send(rejectRssDto);
-
-      const accepted = await rssRejectRepository.findOne({
-        where: { description: REJECT_REASON },
-      });
-
-      // then
-      expect(response.status).toBe(HttpStatus.CREATED);
-      expect(accepted).not.toBeNull();
+  it('[201] 정상적으로 RSS를 거절한다.', async () => {
+    // given
+    const REJECT_REASON = '거절 사유';
+    const rss = await rssRepository.save(RssFixture.createRssFixture());
+    const rejectRssDto = new RejectRssRequestDto({
+      description: REJECT_REASON,
     });
+
+    // when
+    const response = await request(app.getHttpServer())
+      .post(`/api/rss/reject/${rss.id}`)
+      .set('Cookie', 'sessionId=testSessionId')
+      .send(rejectRssDto);
+
+    const accepted = await rssRejectRepository.findOne({
+      where: { description: REJECT_REASON },
+    });
+
+    // then
+    expect(response.status).toBe(HttpStatus.CREATED);
+    expect(accepted).not.toBeNull();
   });
 
-  describe('비정상적인 요청을 한다.', () => {
-    it('[404] 존재하지 않는 rss를 거절할 때', async () => {
-      // given
-      const REJECT_REASON = '거절 사유';
-      const rejectRssDto = new RejectRssRequestDto({
-        description: REJECT_REASON,
-      });
-
-      // when
-      const response = await request(app.getHttpServer())
-        .post(`/api/rss/reject/1`)
-        .set('Cookie', 'sessionId=testSessionId')
-        .send(rejectRssDto);
-
-      // then
-      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+  it('[404] 존재하지 않는 rss를 거절할 때', async () => {
+    // given
+    const REJECT_REASON = '거절 사유';
+    const rejectRssDto = new RejectRssRequestDto({
+      description: REJECT_REASON,
     });
 
-    it('[401] 유효한 세션이 존재하지 않을 때', async () => {
-      // when
-      const noCookieResponse = await request(app.getHttpServer())
-        .post(`/api/rss/reject/1`)
-        .send();
+    // when
+    const response = await request(app.getHttpServer())
+      .post(`/api/rss/reject/1`)
+      .set('Cookie', 'sessionId=testSessionId')
+      .send(rejectRssDto);
 
-      const noSessionResponse = await request(app.getHttpServer())
-        .post(`/api/rss/reject/1`)
-        .set('Cookie', 'sessionId=invalid')
-        .send();
+    // then
+    expect(response.status).toBe(HttpStatus.NOT_FOUND);
+  });
 
-      // then
-      expect(noCookieResponse.status).toBe(HttpStatus.UNAUTHORIZED);
-      expect(noSessionResponse.status).toBe(HttpStatus.UNAUTHORIZED);
-    });
+  it('[401] 유효한 세션이 존재하지 않을 때', async () => {
+    // when
+    const noCookieResponse = await request(app.getHttpServer())
+      .post(`/api/rss/reject/1`)
+      .send();
+
+    const noSessionResponse = await request(app.getHttpServer())
+      .post(`/api/rss/reject/1`)
+      .set('Cookie', 'sessionId=invalid')
+      .send();
+
+    // then
+    expect(noCookieResponse.status).toBe(HttpStatus.UNAUTHORIZED);
+    expect(noSessionResponse.status).toBe(HttpStatus.UNAUTHORIZED);
   });
 });
