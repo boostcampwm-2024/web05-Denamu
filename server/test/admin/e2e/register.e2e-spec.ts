1b7d75ba7ef52cb11086fd441705b12aade1f552
J_Coder <seok3765@naver.com>
2025-11-13 01:34:16 +0900

    ✅ test: 테스트 격리성 향상

--- server/test/admin/e2e/register.e2e-spec.ts
+++ server/test/admin/e2e/register.e2e-spec.ts
@@ -1,10 +1,11 @@
 import { HttpStatus, INestApplication } from '@nestjs/common';
-import { LoginAdminRequestDto } from '../../../src/admin/dto/request/loginAdmin.dto';
 import { RegisterAdminRequestDto } from '../../../src/admin/dto/request/registerAdmin.dto';
 import * as supertest from 'supertest';
 import { AdminFixture } from '../../fixture/admin.fixture';
 import { AdminRepository } from '../../../src/admin/repository/admin.repository';
 import TestAgent from 'supertest/lib/agent';
+import { RedisService } from '../../../src/common/redis/redis.service';
+import { REDIS_KEYS } from '../../../src/common/redis/redis.constant';
 
 describe('POST /api/admin/register E2E Test', () => {
   let app: INestApplication;
@@ -14,23 +15,26 @@ describe('POST /api/admin/register E2E Test', () => {
     app = global.testApp;
     agent = supertest(app.getHttpServer());
     const adminRepository = app.get(AdminRepository);
+    const redisService = app.get(RedisService);
     await adminRepository.insert(await AdminFixture.createAdminCryptFixture());
+    await redisService.set(
+      `${REDIS_KEYS.ADMIN_AUTH_KEY}:testSessionId`,
+      'test1234',
+    );
   });
 
   it('[201] 관리자가 로그인되어 있으면 다른 관리자 계정 회원가입을 할 수 있다.', async () => {
     // given
-    const loginAdminDto = new LoginAdminRequestDto({
-      loginId: 'test1234',
-      password: 'test1234!',
-    });
     const newAdminDto = new RegisterAdminRequestDto({
       loginId: 'testNewAdminId',
       password: 'testNewAdminPassword!',
     });
 
     // when
-    await agent.post('/api/admin/login').send(loginAdminDto);
-    const response = await agent.post('/api/admin/register').send(newAdminDto);
+    const response = await agent
+      .post('/api/admin/register')
+      .send(newAdminDto)
+      .set('Cookie', 'sessionId=testSessionId');
 
     // then
     expect(response.status).toBe(HttpStatus.CREATED);
@@ -38,18 +42,16 @@ describe('POST /api/admin/register E2E Test', () => {
 
   it('[409] 이미 가입한 ID를 입력하면 관리자 계정을 생성할 수 없다.', async () => {
     // given
-    const loginAdminDto = new LoginAdminRequestDto({
-      loginId: 'test1234',
-      password: 'test1234!',
-    });
     const newAdminDto = new RegisterAdminRequestDto({
       loginId: 'testNewAdminId',
       password: 'testNewAdminPassword!',
     });
 
     // when
-    await agent.post('/api/admin/login').send(loginAdminDto);
-    const response = await agent.post('/api/admin/register').send(newAdminDto);
+    const response = await agent
+      .post('/api/admin/register')
+      .send(newAdminDto)
+      .set('Cookie', 'sessionId=testSessionId');
 
     //then
     expect(response.status).toBe(HttpStatus.CONFLICT);
--- server/test/admin/e2e/session-id.e2e-spec.ts
+++ server/test/admin/e2e/session-id.e2e-spec.ts
@@ -1,10 +1,11 @@
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as supertest from 'supertest';
 import { v4 as uuidv4 } from 'uuid';
-import { LoginAdminRequestDto } from '../../../src/admin/dto/request/loginAdmin.dto';
 import { AdminRepository } from '../../../src/admin/repository/admin.repository';
 import { AdminFixture } from '../../fixture/admin.fixture';
 import TestAgent from 'supertest/lib/agent';
+import { RedisService } from '../../../src/common/redis/redis.service';
+import { REDIS_KEYS } from '../../../src/common/redis/redis.constant';
 
 describe('GET /api/admin/sessionId E2E Test', () => {
   let app: INestApplication;
@@ -14,19 +15,19 @@ describe('GET /api/admin/sessionId E2E Test', () => {
     app = global.testApp;
     agent = supertest(app.getHttpServer());
     const adminRepository = app.get(AdminRepository);
+    const redisService = app.get(RedisService);
     await adminRepository.insert(await AdminFixture.createAdminCryptFixture());
+    await redisService.set(
+      `${REDIS_KEYS.ADMIN_AUTH_KEY}:testSessionId`,
+      'test1234',
+    );
   });
 
   it('[200] 쿠키의 session id가 유효하다면 관리자를 로그인 상태로 취급한다.', async () => {
-    // given
-    const requestDto = new LoginAdminRequestDto({
-      loginId: 'test1234',
-      password: 'test1234!',
-    });
-
     // when
-    await agent.post('/api/admin/login').send(requestDto);
-    const response = await agent.get('/api/admin/sessionId');
+    const response = await agent
+      .get('/api/admin/sessionId')
+      .set('Cookie', 'sessionId=testSessionId');
 
     // then
     expect(response.status).toBe(HttpStatus.OK);
@@ -39,7 +40,7 @@ describe('GET /api/admin/sessionId E2E Test', () => {
     // when
     const response = await agent
       .get('/api/admin/sessionId')
-      .set('Cookie', [`sessionId=${randomUUID}`]);
+      .set('Cookie', `sessionId=${randomUUID}`);
 
     // then
     expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
@@ -49,7 +50,7 @@ describe('GET /api/admin/sessionId E2E Test', () => {
     // when
     const response = await agent
       .get('/api/admin/sessionId')
-      .set('Cookie', [`sessionId=`]);
+      .set('Cookie', 'sessionId=');
 
     // then
     expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
