a2528e22c5e037f67d20d24f2caa1dda94f709b0
J_Coder <seok3765@naver.com>
2025-11-21 03:43:30 +0900

    ✅ test: get /api/feed/recent 테스트 코드 cleanup 계층 추가

--- server/test/feed/e2e/recent.e2e-spec.ts
+++ server/test/feed/e2e/recent.e2e-spec.ts
@@ -1,6 +1,5 @@
 import * as supertest from 'supertest';
 import { HttpStatus, INestApplication } from '@nestjs/common';
-import { Feed } from '../../../src/feed/entity/feed.entity';
 import { RssAcceptRepository } from '../../../src/rss/repository/rss.repository';
 import { RssAcceptFixture } from '../../fixture/rss-accept.fixture';
 import { FeedFixture } from '../../fixture/feed.fixture';
@@ -12,12 +11,14 @@ import TestAgent from 'supertest/lib/agent';
 describe('GET /api/feed/recent E2E Test', () => {
   let app: INestApplication;
   let agent: TestAgent;
+  let redisService: RedisService;
   let rssAcceptRepository: RssAcceptRepository;
 
   beforeAll(async () => {
     app = global.testApp;
     agent = supertest(app.getHttpServer());
     rssAcceptRepository = app.get(RssAcceptRepository);
+    redisService = app.get(RedisService);
   });
 
   it('[200] 최신 피드 업데이트 요청이 들어올 경우 최신 피드 제공을 성공한다.', async () => {
@@ -26,16 +27,11 @@ describe('GET /api/feed/recent E2E Test', () => {
       RssAcceptFixture.createRssAcceptFixture(),
     );
     const feedRepository = app.get(FeedRepository);
-    const feedList: Feed[] = [];
-    const baseDate = new Date();
-    for (let i = 0; i < 2; i++) {
-      const date = new Date(baseDate);
+    const feedList = Array.from({ length: 2 }).map((_, i) => {
+      const date = new Date();
       date.setHours(date.getHours() + i);
-      feedList.push(
-        FeedFixture.createFeedFixture(blog, { createdAt: date }, i + 1),
-      );
-    }
-    const redisService = app.get(RedisService);
+      return FeedFixture.createFeedFixture(blog, { createdAt: date }, i + 1);
+    });
     const feeds = await feedRepository.save(feedList);
     await redisService.executePipeline((pipeline) => {
       pipeline.hset(`${REDIS_KEYS.FEED_RECENT_KEY}:${feeds[0].id}`, feeds[0]);
@@ -47,13 +43,21 @@ describe('GET /api/feed/recent E2E Test', () => {
 
     // then
     expect(response.status).toBe(HttpStatus.OK);
-    expect(response.body.data.map((feed) => feed.id)).toStrictEqual(['2', '1']);
+    expect(response.body.data.map((feed) => feed.id)).toStrictEqual([
+      feeds[1].id.toString(),
+      feeds[0].id.toString(),
+    ]);
+
+    // cleanup
+    await redisService.executePipeline((pipeline) => {
+      pipeline.del(`${REDIS_KEYS.FEED_RECENT_KEY}:${feeds[0].id}`);
+      pipeline.del(`${REDIS_KEYS.FEED_RECENT_KEY}:${feeds[1].id}`);
+    });
   });
 
   it('[200] 최신 피드가 없을 경우 빈 배열 제공을 성공한다.', async () => {
     // given
-    const redisService = app.get(RedisService);
-    redisService.flushdb();
+    await redisService.flushdb();
 
     // when
     const response = await agent.get('/api/feed/recent');
