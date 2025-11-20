e6d7d6a5bb1c28580beefc285e05813a5675330f
codeVac513 <xssdsw@naver.com>
2025-11-21 00:55:17 +0900

    ✨ feat: 의존성 주입을 위한 DI 컨테이너 설정

---
+++ email-worker/src/types/dependency-symbols.ts
@@ -0,0 +1,7 @@
+export const DEPENDENCY_SYMBOLS = {
+  RabbitMQService: Symbol.for('RabbitMQService'),
+  RabbitMQConfig: Symbol.for('RabbitMQConfig'),
+  RabbitMQManager: Symbol.for('RabbitMQManager'),
+  EmailConsumer: Symbol.for('EmailConsumer'),
+  EmailService: Symbol.for('EmailService'),
+};
