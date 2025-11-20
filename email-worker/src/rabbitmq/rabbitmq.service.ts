da05e5629f8374848ddc3aae8a322a7b0d0cbf68
codeVac513 <xssdsw@naver.com>
2025-11-21 00:50:40 +0900

    ✨ feat: rabbitmq 관련 기능 추가

---
+++ email-worker/src/rabbitmq/rabbitmq.service.ts
@@ -0,0 +1,52 @@
+import { inject, injectable } from 'tsyringe';
+import { RabbitMQManager } from './rabbitmq.manager';
+import { ConsumeMessage } from 'amqplib/properties';
+import { DEPENDENCY_SYMBOLS } from '../types/dependency-symbols';
+import logger from '../logger';
+
+@injectable()
+export class RabbitmqService {
+  private nameTag: string;
+
+  constructor(
+    @inject(DEPENDENCY_SYMBOLS.RabbitMQManager)
+    private readonly rabbitMQManager: RabbitMQManager,
+  ) {
+    this.nameTag = '[RabbitMQ]';
+  }
+
+  async sendMessage(exchange: string, routingKey: string, message: string) {
+    const channel = await this.rabbitMQManager.getChannel();
+    channel.publish(exchange, routingKey, Buffer.from(message));
+  }
+
+  async consumeMessage<T>(
+    queue: string,
+    onMessage: (payload: T) => void | Promise<void>,
+  ) {
+    const channel = await this.rabbitMQManager.getChannel();
+    const { consumerTag } = await channel.consume(queue, async (message) => {
+      try {
+        if (!message) return;
+
+        const parsedMessage = JSON.parse(message.content.toString()) as T;
+        await onMessage(parsedMessage);
+
+        channel.ack(message);
+      } catch (error) {
+        logger.error(
+          `${this.nameTag} 메시지 처리 중 오류 발생
+          오류 메시지: ${error.message}
+          스택 트레이스: ${error.stack}`,
+        );
+        channel.nack(message, false, false);
+      }
+    });
+    return consumerTag;
+  }
+
+  async closeConsumer(consumerTag: string) {
+    const channel = await this.rabbitMQManager.getChannel();
+    await channel.cancel(consumerTag);
+  }
+}
