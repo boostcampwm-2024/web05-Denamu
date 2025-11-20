6e078a3b7dae336021a4608ecc8033abb9b3e9e3
codeVac513 <xssdsw@naver.com>
2025-11-21 01:01:33 +0900

    ✨ feat: rabbitmq service 사용 시 제네릭을 통한 message 타입 선언 유연성 추가

--- server/src/common/rabbitmq/rabbitmq.service.ts
+++ server/src/common/rabbitmq/rabbitmq.service.ts
@@ -10,28 +10,35 @@ export class RabbitMQService {
     private readonly logger: WinstonLoggerService,
   ) {}
 
-  async sendMessage(exchange: string, routingKey: string, message: string) {
+  async sendMessage<T>(exchange: string, routingKey: string, message: T) {
     const channel = await this.rabbitMQManager.getChannel();
-    channel.publish(exchange, routingKey, Buffer.from(message));
+    const stringifiedMessage = JSON.stringify(message);
+    channel.publish(exchange, routingKey, Buffer.from(stringifiedMessage));
   }
 
-  async consumeMessage(
+  async consumeMessage<T>(
     queue: string,
-    onMessage: (msg: ConsumeMessage | null) => void | Promise<void>,
+    onMessage: (payload: T) => void | Promise<void>,
   ) {
     const channel = await this.rabbitMQManager.getChannel();
-    await channel.consume(queue, async (message) => {
+    const { consumerTag } = await channel.consume(queue, async (message) => {
       try {
         if (!message) return;
 
-        const parsedMessage = JSON.parse(message.content.toString());
+        const parsedMessage = JSON.parse(message.content.toString()) as T;
         await onMessage(parsedMessage);
 
         channel.ack(message);
-      } catch (err) {
-        this.logger.error('메시지 처리 중 오류 발생:', err);
+      } catch (error) {
+        this.logger.error('메시지 처리 중 오류 발생:', error);
         channel.nack(message, false, false);
       }
     });
+    return consumerTag;
+  }
+
+  async closeConsumer(consumerTag: string) {
+    const channel = await this.rabbitMQManager.getChannel();
+    await channel.cancel(consumerTag);
   }
 }
