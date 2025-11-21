68a303bff42f5795362f704dc7cdc5c5e458c97c
J_Coder <seok3765@naver.com>
2025-11-22 04:30:42 +0900

    ✅ test: 페이지네이션 테스트 코드 결과값 검증 추가

--- server/test/feed/e2e/pagination.e2e-spec.ts
+++ server/test/feed/e2e/pagination.e2e-spec.ts
@@ -6,11 +6,12 @@ import { RssAcceptRepository } from '../../../src/rss/repository/rss.repository'
 import { RssAcceptFixture } from '../../fixture/rss-accept.fixture';
 import { ReadFeedPaginationRequestDto } from '../../../src/feed/dto/request/readFeedPagination.dto';
 import TestAgent from 'supertest/lib/agent';
+import { Feed } from '../../../src/feed/entity/feed.entity';
 
 describe('GET /api/feed?limit={}&lastId={} E2E Test', () => {
   let app: INestApplication;
   let agent: TestAgent;
-  const latestId = 20;
+  let feedList: Feed[];
 
   beforeAll(async () => {
     app = global.testApp;
@@ -18,15 +19,16 @@ describe('GET /api/feed?limit={}&lastId={} E2E Test', () => {
     const feedRepository = app.get(FeedRepository);
     const rssAcceptRepository = app.get(RssAcceptRepository);
 
-    const blog = await rssAcceptRepository.save(
+    const rssAccept = await rssAcceptRepository.save(
       RssAcceptFixture.createRssAcceptFixture(),
     );
 
-    const feeds = Array.from({ length: latestId }).map((_, i) =>
-      FeedFixture.createFeedFixture(blog, _, i + 1),
+    const feeds = Array.from({ length: 10 }).map((_, i) =>
+      FeedFixture.createFeedFixture(rssAccept, _, i + 1),
     );
 
-    await feedRepository.insert(feeds);
+    // 최신 게시글부터 제공하기에 테스트 편의성을 위해 최신 게시글을 앞으로
+    feedList = (await feedRepository.save(feeds)).reverse();
   });
 
   it('[200] 마지막 수신 피드 ID가 없을 경우 최신 피드부터 피드 목록 제공을 성공한다.', async () => {
@@ -39,82 +41,130 @@ describe('GET /api/feed?limit={}&lastId={} E2E Test', () => {
     const response = await agent.get('/api/feed').query(requestDto);
 
     // then
+    const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
-    expect(response.body.data.result.map((feed) => feed.id)).toStrictEqual([
-      latestId,
-      latestId - 1,
-      latestId - 2,
-      latestId - 3,
-      latestId - 4,
-    ]);
-    expect(response.body.data.hasMore).toBe(true);
-    expect(response.body.data.lastId).toBe(16);
+    expect(data).toStrictEqual({
+      result: Array.from({ length: requestDto.limit }).map((_, i) => {
+        const index = 0 + i;
+        const feed = feedList[index];
+
+        return {
+          id: feed.id,
+          author: feed.blog.name,
+          blogPlatform: feed.blog.blogPlatform,
+          title: feed.title,
+          path: feed.path,
+          createdAt: feed.createdAt.toISOString(),
+          thumbnail: feed.thumbnail,
+          viewCount: feed.viewCount,
+          tag: [],
+          likes: feed.likeCount,
+          comments: feed.commentCount,
+          isNew: false,
+        };
+      }),
+      lastId: feedList[requestDto.limit - 1].id,
+      hasMore: true,
+    });
   });
 
   it('[200] 마지막 수신 피드 ID가 있을 경우 마지막 수신 피드 이후의 피드 목록 제공을 성공한다.', async () => {
     // given
     const requestDto = new ReadFeedPaginationRequestDto({
-      limit: 5,
-      lastId: 11,
+      limit: 3,
+      lastId: feedList[3].id,
     });
 
     // when
     const response = await agent.get('/api/feed').query(requestDto);
 
     // then
+    const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
-    expect(response.body.data.result.map((feed) => feed.id)).toStrictEqual([
-      requestDto.lastId - 1,
-      requestDto.lastId - 2,
-      requestDto.lastId - 3,
-      requestDto.lastId - 4,
-      requestDto.lastId - 5,
-    ]);
-    expect(response.body.data.hasMore).toBe(true);
-    expect(response.body.data.lastId).toBe(6);
+    expect(data).toStrictEqual({
+      result: Array.from({ length: requestDto.limit }).map((_, i) => {
+        const index = 4 + i;
+        const feed = feedList[index];
+
+        return {
+          id: feed.id,
+          author: feed.blog.name,
+          blogPlatform: feed.blog.blogPlatform,
+          title: feed.title,
+          path: feed.path,
+          createdAt: feed.createdAt.toISOString(),
+          thumbnail: feed.thumbnail,
+          viewCount: feed.viewCount,
+          tag: [],
+          likes: feed.likeCount,
+          comments: feed.commentCount,
+          isNew: false,
+        };
+      }),
+      lastId: feedList[6].id,
+      hasMore: true,
+    });
   });
 
   it('[200] 받고자 하는 수신 피드 개수가 남은 피드 개수보다 적을 경우 남은 모든 피드 목록 제공을 성공한다.', async () => {
     // given
     const requestDto = new ReadFeedPaginationRequestDto({
-      limit: 15,
-      lastId: 10,
+      limit: 10,
+      lastId: feedList[5].id,
     });
 
     // when
     const response = await agent.get('/api/feed').query(requestDto);
 
     // then
+    const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
-    expect(response.body.data.result.map((feed) => feed.id)).toStrictEqual([
-      requestDto.lastId - 1,
-      requestDto.lastId - 2,
-      requestDto.lastId - 3,
-      requestDto.lastId - 4,
-      requestDto.lastId - 5,
-      requestDto.lastId - 6,
-      requestDto.lastId - 7,
-      requestDto.lastId - 8,
-      requestDto.lastId - 9,
-    ]);
-    expect(response.body.data.hasMore).toBe(false);
-    expect(response.body.data.lastId).toBe(1);
+    expect(data).toStrictEqual({
+      result: Array.from({ length: requestDto.limit })
+        .map((_, i) => {
+          const index = 6 + i;
+          const feed = feedList[index];
+
+          return !feed
+            ? null
+            : {
+                id: feed.id,
+                author: feed.blog.name,
+                blogPlatform: feed.blog.blogPlatform,
+                title: feed.title,
+                path: feed.path,
+                createdAt: feed.createdAt.toISOString(),
+                thumbnail: feed.thumbnail,
+                viewCount: feed.viewCount,
+                tag: [],
+                likes: feed.likeCount,
+                comments: feed.commentCount,
+                isNew: false,
+              };
+        })
+        .filter((value) => value),
+      lastId: feedList[feedList.length - 1].id,
+      hasMore: false,
+    });
   });
 
   it('[200] 남은 피드 개수가 없을 경우 빈 배열과 마지막 피드 ID를 0으로 제공을 성공한다.', async () => {
     // given
     const requestDto = new ReadFeedPaginationRequestDto({
       limit: 15,
-      lastId: 1,
+      lastId: feedList[feedList.length - 1].id,
     });
 
     // when
     const response = await agent.get('/api/feed').query(requestDto);
 
     // then
+    const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
-    expect(response.body.data.result.map((feed) => feed.id)).toStrictEqual([]);
-    expect(response.body.data.hasMore).toBe(false);
-    expect(response.body.data.lastId).toBe(0);
+    expect(data).toStrictEqual({
+      result: [],
+      lastId: 0,
+      hasMore: false,
+    });
   });
 });
