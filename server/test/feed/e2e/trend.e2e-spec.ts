8acc8ac9700f0abf0ff8284e5783c6581b747070
J_Coder <seok3765@naver.com>
2025-11-23 01:44:12 +0900

    ✅ test: 트렌드 SSE 테스트 코드 결과값 검증 추가

--- server/test/feed/e2e/trend.e2e-spec.ts
+++ server/test/feed/e2e/trend.e2e-spec.ts
@@ -6,53 +6,48 @@ import { FeedRepository } from '../../../src/feed/repository/feed.repository';
 import { RssAcceptRepository } from '../../../src/rss/repository/rss.repository';
 import { FeedFixture } from '../../fixture/feed.fixture';
 import * as EventSource from 'eventsource';
+import { Feed } from '../../../src/feed/entity/feed.entity';
 
 describe('SSE /api/trend/sse E2E Test', () => {
   let app: INestApplication;
-  let feedRepository: FeedRepository;
   let serverUrl: string;
+  let feedList: Feed[];
 
   beforeAll(async () => {
     app = global.testApp;
-    feedRepository = app.get(FeedRepository);
-    const rssAcceptRepository = app.get(RssAcceptRepository);
-    const redisService = app.get(RedisService);
-    const blog = await rssAcceptRepository.save(
-      RssAcceptFixture.createRssAcceptFixture(),
-    );
-    const feedData = Array.from({ length: 2 }).map((_, i) =>
-      FeedFixture.createFeedFixture(blog, _, i + 1),
-    );
-    const feedList = await feedRepository.save(feedData);
-    const [httpServer] = await Promise.all([
-      app.listen(0),
-      redisService.rpush(
-        REDIS_KEYS.FEED_ORIGIN_TREND_KEY,
-        feedList[0].id,
-        feedList[1].id,
-      ),
-    ]);
+    const httpServer = await app.listen(0);
     const port = httpServer.address().port;
     serverUrl = `http://localhost:${port}/api/feed/trend/sse`;
   });
 
   it('[SSE] 최초 연결을 할 경우 트랜드 데이터 최대 4개 제공 수신을 성공한다.', async () => {
     // given
-    const es = new EventSource(serverUrl);
-    const timeout = new Promise((_, reject) =>
-      setTimeout(
-        () => reject(new Error('Timeout occurred before receiving data')),
-        1000,
-      ),
+    let data: any;
+    const feedRepository = app.get(FeedRepository);
+    const rssAcceptRepository = app.get(RssAcceptRepository);
+    const redisService = app.get(RedisService);
+    const rssAccept = await rssAcceptRepository.save(
+      RssAcceptFixture.createRssAcceptFixture(),
     );
+    const feeds = Array.from({ length: 2 }).map((_, i) =>
+      FeedFixture.createFeedFixture(rssAccept, _, i + 1),
+    );
+    feedList = await feedRepository.save(feeds);
+    await redisService.rpush(
+      REDIS_KEYS.FEED_ORIGIN_TREND_KEY,
+      feedList[0].id,
+      feedList[1].id,
+    );
+
+    // when
+    const es = new EventSource(serverUrl);
     const eventResult = new Promise((resolve, reject) => {
       es.onmessage = (event) => {
         try {
           const response = JSON.parse(event.data);
-          const feedList = response.data;
-          const idList = feedList.map((feed) => feed.id);
+          data = response.data;
           es.close();
-          resolve(idList);
+          resolve(null);
         } catch (error) {
           es.close();
           reject(error);
@@ -64,31 +59,45 @@ describe('SSE /api/trend/sse E2E Test', () => {
       };
     });
 
-    // when
-    const idList = await Promise.race([eventResult, timeout]);
+    await eventResult;
 
     // then
-    expect(idList).toStrictEqual([1, 2]);
+    expect(data).toStrictEqual(
+      Array.from({ length: 2 }).map((_, i) => {
+        const feed = feedList[i];
+        return {
+          id: feed.id,
+          author: feed.blog.name,
+          blogPlatform: feed.blog.blogPlatform,
+          title: feed.title,
+          path: feed.path,
+          createdAt: feed.createdAt.toISOString(),
+          thumbnail: feed.thumbnail,
+          viewCount: feed.viewCount,
+          likes: feed.likeCount,
+          comments: feed.commentCount,
+          tag: [],
+        };
+      }),
+    );
+
+    // cleanup
+    await feedRepository.delete(feedList.map((_, i) => feedList[i].id));
   });
 
   it('[SSE] 서버로부터 데이터를 받을 때 게시글이 데나무에서 지워진 경우 빈 피드 정보 수신을 성공한다.', async () => {
     // given
-    await feedRepository.delete({ id: 2 });
+    let data: any;
+
+    // when
     const es = new EventSource(serverUrl);
-    const timeout = new Promise((_, reject) =>
-      setTimeout(
-        () => reject(new Error('Timeout occurred before receiving data')),
-        1000,
-      ),
-    );
     const eventResult = new Promise((resolve, reject) => {
       es.onmessage = (event) => {
         try {
           const response = JSON.parse(event.data);
-          const feedList = response.data;
-          const idList = feedList.map((feed) => feed.id);
+          data = response.data;
           es.close();
-          resolve(idList);
+          resolve(null);
         } catch (error) {
           es.close();
           reject(error);
@@ -100,10 +109,9 @@ describe('SSE /api/trend/sse E2E Test', () => {
       };
     });
 
-    // when
-    const idList = await Promise.race([eventResult, timeout]);
+    await eventResult;
 
     // then
-    expect(idList).toStrictEqual([1]);
+    expect(data).toStrictEqual([]);
   });
 });
