3e3e0af34ea0de4fa932d8284fd7941333b7048c
J_Coder <seok3765@naver.com>
2025-11-24 00:57:39 +0900

    ✅ test: rss 신청 조회 테스트 코드 결과 검증값 추가

--- server/test/rss/e2e/get.e2e-spec.ts
+++ server/test/rss/e2e/get.e2e-spec.ts
@@ -29,8 +29,9 @@ describe('GET /api/rss E2E Test', () => {
     const response = await agent.get('/api/rss');
 
     // then
+    const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
-    expect(response.body.data).toEqual([]);
+    expect(data).toStrictEqual([]);
   });
 
   it('[200] 신청된 RSS가 있을 경우 RSS 신청 조회를 성공한다.', async () => {
@@ -41,7 +42,16 @@ describe('GET /api/rss E2E Test', () => {
     const response = await agent.get('/api/rss');
 
     //then
+    const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
-    expect(response.body.data).toEqual([rss]);
+    expect(data).toStrictEqual([
+      {
+        id: rss.id,
+        name: rss.name,
+        userName: rss.userName,
+        email: rss.email,
+        rssUrl: rss.rssUrl,
+      },
+    ]);
   });
 });
