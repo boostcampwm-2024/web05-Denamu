7bc526a7cbf52f42938250df6465e6fd79899be8
J_Coder <seok3765@naver.com>
2025-11-23 20:56:44 +0900

    ✅ test: 좋아요 조회 테스트 코드 결과값 검증 추가

--- server/test/like/e2e/get.e2e-spec.ts
+++ server/test/like/e2e/get.e2e-spec.ts
@@ -9,12 +9,17 @@ import { RssAccept } from '../../../src/rss/entity/rss.entity';
 import { Feed } from '../../../src/feed/entity/feed.entity';
 import * as supertest from 'supertest';
 import TestAgent from 'supertest/lib/agent';
+import { User } from '../../../src/user/entity/user.entity';
+import { UserService } from '../../../src/user/service/user.service';
+import { LikeRepository } from '../../../src/like/repository/like.repository';
 
 describe('GET /api/like/{feedId} E2E Test', () => {
   let app: INestApplication;
   let agent: TestAgent;
   let rssAccept: RssAccept;
   let feed: Feed;
+  let user: User;
+  let userService: UserService;
 
   beforeAll(async () => {
     app = global.testApp;
@@ -22,12 +27,17 @@ describe('GET /api/like/{feedId} E2E Test', () => {
     const userRepository = app.get(UserRepository);
     const rssAcceptRepository = app.get(RssAcceptRepository);
     const feedRepository = app.get(FeedRepository);
+    const likeRepository = app.get(LikeRepository);
+    userService = app.get(UserService);
 
-    await userRepository.save(await UserFixture.createUserCryptFixture());
+    user = await userRepository.save(
+      await UserFixture.createUserCryptFixture(),
+    );
     rssAccept = await rssAcceptRepository.save(
       RssAcceptFixture.createRssAcceptFixture(),
     );
     feed = await feedRepository.save(FeedFixture.createFeedFixture(rssAccept));
+    await likeRepository.save({ user, feed });
   });
 
   it('[404] 게시글이 존재하지 않을 경우 좋아요 정보 제공을 실패한다.', async () => {
@@ -38,11 +48,40 @@ describe('GET /api/like/{feedId} E2E Test', () => {
     expect(response.status).toBe(HttpStatus.NOT_FOUND);
   });
 
-  it('[200] 게시글에 대한 좋아요 조회 요청을 받을 경우 좋아요 정보 제공을 성공한다.', async () => {
+  it('[200] 로그인하지 않은 상황에서 게시글에 대한 좋아요 조회 요청을 받을 경우 좋아요 정보 제공을 성공한다.', async () => {
     // when
     const response = await agent.get(`/api/like/${feed.id}`);
 
     // then
+    const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
+    expect(data).toStrictEqual({
+      isLike: false,
+    });
+  });
+
+  it('[200] 로그인한 상황에서 게시글에 대한 좋아요 조회 요청을 받을 경우 좋아요 정보 제공을 성공한다.', async () => {
+    // given
+    const accessToken = userService.createToken(
+      {
+        id: user.id,
+        email: user.email,
+        userName: user.userName,
+        role: 'user',
+      },
+      'access',
+    );
+
+    // when
+    const response = await agent
+      .get(`/api/like/${feed.id}`)
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    // then
+    const { data } = response.body;
+    expect(response.status).toBe(HttpStatus.OK);
+    expect(data).toStrictEqual({
+      isLike: true,
+    });
   });
 });
