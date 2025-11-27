9e1c32097e78d01e7d990064007bd494b3bf237d
J_Coder <seok3765@naver.com>
2025-11-28 03:03:46 +0900

    ✅ test: 유저 회원 가입 인증 테스트 코드 DB 검증 추가

--- server/test/user/e2e/certificate.e2e-spec.ts
+++ server/test/user/e2e/certificate.e2e-spec.ts
@@ -5,6 +5,8 @@ import { UserFixture } from '../../fixture/user.fixture';
 import { REDIS_KEYS } from '../../../src/common/redis/redis.constant';
 import { CertificateUserRequestDto } from '../../../src/user/dto/request/certificateUser.dto';
 import TestAgent from 'supertest/lib/agent';
+import { UserRepository } from '../../../src/user/repository/user.repository';
+import * as bcrypt from 'bcrypt';
 
 const URL = '/api/user/certificate';
 
@@ -12,11 +14,13 @@ describe(`POST ${URL} E2E Test`, () => {
   let app: INestApplication;
   let agent: TestAgent;
   let redisService: RedisService;
+  let userRepository: UserRepository;
 
   beforeAll(async () => {
     app = global.testApp;
     agent = supertest(app.getHttpServer());
     redisService = app.get(RedisService);
+    userRepository = app.get(UserRepository);
   });
 
   it('[404] 존재하지 않거나 만료된 UUID로 인증을 요청할 경우 회원 가입 인증을 실패한다.', async () => {
@@ -25,10 +29,10 @@ describe(`POST ${URL} E2E Test`, () => {
       uuid: 'non-existent-or-expired-uuid',
     });
 
-    // when
+    // Http when
     const response = await agent.post(URL).send(requestDto);
 
-    // then
+    // Http then
     const { data } = response.body;
     expect(response.status).toBe(HttpStatus.NOT_FOUND);
     expect(data).toBeUndefined();
@@ -37,17 +41,34 @@ describe(`POST ${URL} E2E Test`, () => {
   it('[200] 올바른 UUID로 인증을 요청할 경우 회원 가입 인증을 성공한다.', async () => {
     // given
     const uuid = 'test-certificate-uuid';
-    const userEntity = UserFixture.createUserFixture();
+    const userEntity = await UserFixture.createUserCryptFixture();
     const redisKey = `${REDIS_KEYS.USER_AUTH_KEY}:${uuid}`;
     const requestDto = new CertificateUserRequestDto({ uuid });
     await redisService.set(redisKey, JSON.stringify(userEntity));
 
-    // when
+    // Http when
     const response = await agent.post(URL).send(requestDto);
 
-    // then
+    // Http then
     const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
     expect(data).toBeUndefined();
+
+    // DB, Redis when
+    const savedUser = await userRepository.findOneBy({
+      email: userEntity.email,
+      userName: userEntity.userName,
+    });
+    const savedUUID = await redisService.get(redisKey);
+
+    // DB, Redis then
+    expect(savedUUID).toBeNull();
+    expect(savedUser).not.toBeUndefined();
+    expect(
+      await bcrypt.compare(
+        UserFixture.GENERAL_USER.password,
+        savedUser.password,
+      ),
+    ).toBeTruthy();
   });
 });
