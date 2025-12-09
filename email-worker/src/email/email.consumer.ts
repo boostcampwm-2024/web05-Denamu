905c8a3b347dc64f48e0dfbdcd60828b15774ae0
CodeVac513 <xssdsw@naver.com>
2025-12-09 16:03:13 +0900

    ✨ feat: error 종류에 따라 재시도하거나 DLQ로 메시지를 발행할 수 있도록 기능 구현

--- email-worker/src/email/email.consumer.ts
+++ email-worker/src/email/email.consumer.ts
@@ -3,8 +3,9 @@ import { inject, injectable } from 'tsyringe';
 import { DEPENDENCY_SYMBOLS } from '../types/dependency-symbols';
 import { EmailService } from './email.service';
 import logger from '../logger';
-import { RMQ_QUEUES } from '../rabbitmq/rabbitmq.constant';
+import { RETRY_CONFIG, RMQ_QUEUES } from '../rabbitmq/rabbitmq.constant';
 import { EmailPayload, EmailPayloadConstant } from '../types/types';
+import { Options } from 'amqplib/properties';
 
 @injectable()
 export class EmailConsumer {
@@ -25,7 +26,7 @@ export class EmailConsumer {
 
     this.consumerTag = await this.rabbitmqService.consumeMessage(
       RMQ_QUEUES.EMAIL_SEND,
-      async (payload: EmailPayload) => {
+      async (payload: EmailPayload, retryCount: number) => {
         if (this.shuttingDownFlag) {
           logger.warn('[EmailConsumer] Shutdown 중, 메시지 처리 건너뜀');
           throw new Error('SHUTDOWN_IN_PROGRESS');
@@ -39,6 +40,8 @@ export class EmailConsumer {
         try {
           await this.handleEmailByType(payload);
           logger.info('[EmailConsumer] 이메일 전송 완료');
+        } catch (error) {
+          await this.handleEmailByError(error, payload, retryCount);
         } finally {
           this.pendingTasks--;
           logger.info(`[EmailConsumer] 남은 작업: ${this.pendingTasks}`);
@@ -119,5 +122,119 @@ export class EmailConsumer {
         resolve();
       }, 10000);
     });
+  }
+
+  async handleEmailByError(
+    error: any,
+    payload: EmailPayload,
+    retryCount: number,
+  ) {
+    const stringifiedMessage = JSON.stringify(payload);
+    const retryOptions: Options.Publish = {
+      headers: {
+        'x-retry-count': retryCount + 1,
+      },
+    };
+
+    // Node.js 네트워크 레벨의 에러
+    const isNetworkError =
+      error.code === 'ESOCKET' ||
+      error.message?.includes('ECONNREFUSED') ||
+      error.message?.includes('ETIMEDOUT') ||
+      error.message?.includes('Unexpected socket close');
+    if (isNetworkError) {
+      if (retryCount >= RETRY_CONFIG.MAX_RETRY) {
+        await this.rabbitmqService.sendMessageToQueue(
+          RMQ_QUEUES.EMAIL_DEAD_LETTER,
+          stringifiedMessage,
+          {
+            headers: {
+              'x-retry-count': retryCount,
+              'x-error-code': error.code || 'NONE',
+              'x-error-message': error.message,
+              'x-failed-at': new Date().toISOString(),
+              'x-failure-type': 'MAX_RETRIES_EXCEEDED',
+            },
+          },
+        );
+        return;
+      }
+
+      await this.rabbitmqService.sendMessageToQueue(
+        RETRY_CONFIG.WAITING_QUEUE[retryCount],
+        stringifiedMessage,
+        retryOptions,
+      );
+      return;
+    }
+    // SMTP 레벨의 에러
+    if (error.responseCode) {
+      if (error.responseCode >= 500) {
+        // DLQ로 보내기
+        await this.rabbitmqService.sendMessageToQueue(
+          RMQ_QUEUES.EMAIL_DEAD_LETTER,
+          stringifiedMessage,
+          {
+            headers: {
+              'x-retry-count': retryCount,
+              'x-error-code': error.code || 'NONE',
+              'x-response-code': error.responseCode,
+              'x-error-message': error.message,
+              'x-failed-at': new Date().toISOString(),
+              'x-failure-type': 'SMTP_PERMANENT_FAILURE',
+            },
+          },
+        );
+        return;
+      }
+
+      if (error.responseCode >= 400) {
+        if (retryCount >= RETRY_CONFIG.MAX_RETRY) {
+          // DLQ로 보내기
+          await this.rabbitmqService.sendMessageToQueue(
+            RMQ_QUEUES.EMAIL_DEAD_LETTER,
+            stringifiedMessage,
+            {
+              headers: {
+                'x-retry-count': retryCount,
+                'x-error-code': error.code || 'NONE',
+                'x-response-code': error.responseCode,
+                'x-error-message': error.message,
+                'x-failed-at': new Date().toISOString(),
+                'x-failure-type': 'MAX_RETRIES_EXCEEDED',
+              },
+            },
+          );
+          return;
+        }
+        await this.rabbitmqService.sendMessageToQueue(
+          RETRY_CONFIG.WAITING_QUEUE[retryCount],
+          stringifiedMessage,
+          retryOptions,
+        );
+        return;
+      }
+    }
+
+    logger.error(
+      `[EmailConsumer] 알 수 없는 에러로 DLQ 전송 
+      오류 메시지: ${error.message} 
+      스택 트레이스: ${error.stack}`,
+    );
+    // 즉시 DLQ로
+    await this.rabbitmqService.sendMessageToQueue(
+      RMQ_QUEUES.EMAIL_DEAD_LETTER,
+      stringifiedMessage,
+      {
+        headers: {
+          'x-retry-count': retryCount,
+          'x-error-code': error.code || 'UNKNOWN',
+          'x-error-message': error.message || 'Unknown error',
+          'x-error-stack': error.stack,
+          'x-failed-at': new Date().toISOString(),
+          'x-failure-type': 'UNKNOWN_ERROR',
+        },
+      },
+    );
   }
 }
