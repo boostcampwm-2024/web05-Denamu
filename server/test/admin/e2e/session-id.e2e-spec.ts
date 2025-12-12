ff21db623e574e23157071ffbe344c406a573e8a
J_Coder <seok3765@naver.com>
2025-12-12 11:24:33 +0900

    ✅ test: session id 테스트 코드 추가

--- server/test/admin/e2e/session-id.e2e-spec.ts
+++ server/test/admin/e2e/session-id.e2e-spec.ts
@@ -13,14 +13,15 @@ describe(`GET ${URL} E2E Test`, () => {
   let app: INestApplication;
   let agent: TestAgent;
   let admin: Admin;
+  let redisService: RedisService;
   const sessionKey = 'admin-session-check-key';
   const redisKeyMake = (data: string) => `${REDIS_KEYS.ADMIN_AUTH_KEY}:${data}`;
 
   beforeAll(async () => {
     app = global.testApp;
     agent = supertest(app.getHttpServer());
+    redisService = app.get(RedisService);
     const adminRepository = app.get(AdminRepository);
-    const redisService = app.get(RedisService);
 
     admin = await adminRepository.save(
       await AdminFixture.createAdminCryptFixture(),
@@ -36,6 +37,12 @@ describe(`GET ${URL} E2E Test`, () => {
     const { data } = response.body;
     expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
     expect(data).toBeUndefined();
+
+    // DB, Redis when
+    const savedSession = await redisService.get(redisKeyMake(sessionKey));
+
+    // DB, Redis then
+    expect(savedSession).not.toBeNull();
   });
 
   it('[401] 관리자 로그인 쿠키가 만료됐을 경우 관리자 자동 로그인을 실패한다.', async () => {
@@ -48,6 +55,12 @@ describe(`GET ${URL} E2E Test`, () => {
     const { data } = response.body;
     expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
     expect(data).toBeUndefined();
+
+    // DB, Redis when
+    const savedSession = await redisService.get(redisKeyMake(sessionKey));
+
+    // DB, Redis then
+    expect(savedSession).not.toBeNull();
   });
 
   it('[200] 관리자 로그인 쿠키가 존재할 경우 관리자 자동 로그인을 성공한다.', async () => {
@@ -60,5 +73,11 @@ describe(`GET ${URL} E2E Test`, () => {
     const { data } = response.body;
     expect(response.status).toBe(HttpStatus.OK);
     expect(data).toBeUndefined();
+
+    // DB, Redis when
+    const savedSession = await redisService.get(redisKeyMake(sessionKey));
+
+    // DB, Redis then
+    expect(savedSession).not.toBeNull();
   });
 });
