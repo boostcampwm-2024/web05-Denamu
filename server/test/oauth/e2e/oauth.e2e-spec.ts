1d71ccb805610cf68a4212ad1f06eff4daf4de67
J_Coder <seok3765@naver.com>
2025-12-27 16:30:01 +0900

    ♻️ refactor: oauth e2e helper 적용

--- server/test/oauth/e2e/oauth.e2e-spec.ts
+++ server/test/oauth/e2e/oauth.e2e-spec.ts
@@ -1,22 +1,12 @@
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import { OAuthTypeRequestDto } from '../../../src/user/dto/request/oAuthType.dto';
 import { OAuthType } from '../../../src/user/constant/oauth.constant';
-import * as supertest from 'supertest';
-import { OAuthService } from '../../../src/user/service/oauth.service';
-import TestAgent from 'supertest/lib/agent';
+import { OAuthE2EHelper } from '../../config/common/helper/oauth/oauth-helper';
 
 const URL = '/api/oauth';
 
 describe(`GET ${URL}?type={} E2E Test`, () => {
-  let app: INestApplication;
-  let agent: TestAgent;
-  let oauthService: OAuthService;
-
-  beforeAll(() => {
-    app = global.testApp;
-    agent = supertest(app.getHttpServer());
-    oauthService = app.get(OAuthService);
-  });
+  const { agent, oauthService } = new OAuthE2EHelper();
 
   it('[302] 올바른 제공자를 입력했을 경우 리다이렉트를 성공한다.', async () => {
     // given
