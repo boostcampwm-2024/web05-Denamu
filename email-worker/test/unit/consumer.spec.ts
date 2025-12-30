3a1167c7753b2aee564dc1b57a9f191db38ab2e1
CodeVac513 <xssdsw@naver.com>
2025-12-30 17:03:23 +0900

    ✅ test: test 가독성을 위해 재시도 로직과 error 배열 분리

--- email-worker/test/unit/consumer.spec.ts
+++ email-worker/test/unit/consumer.spec.ts
@@ -10,6 +10,26 @@ describe('email consumer unit test', () => {
     let emailConsumer: EmailConsumer;
     let rabbitmqService: jest.Mocked<RabbitmqService>;
     let emailService: jest.Mocked<EmailService>;
+    const networkErrors = [
+      `ESOCKET`,
+      `ECONNREFUSED`,
+      `ETIMEDOUT`,
+      `Unexpected socket close`,
+    ];
+    const commonSmtp4xxErrors = [
+      {
+        responseCode: 421,
+        message: 'Service not available, closing transmission channel',
+      },
+      { responseCode: 450, message: 'Mailbox unavailable' },
+      { responseCode: 451, message: 'Local error in processing' },
+      { responseCode: 452, message: 'Insufficient system storage' },
+    ];
+    const commonSmtp5xxErrors = [
+      { responseCode: 550, message: 'Mailbox unavailable' },
+      { responseCode: 552, message: 'Exceeded storage allocation' },
+      { responseCode: 554, message: 'Transaction failed' },
+    ];
     beforeEach(() => {
       emailService = {} as any;
       rabbitmqService = {
@@ -24,12 +44,6 @@ describe('email consumer unit test', () => {
 
     describe('재시도 가능한 에러', () => {
       // Node.js 네트워크 레벨의 에러
-      const networkErrors = [
-        `ESOCKET`,
-        `ECONNREFUSED`,
-        `ETIMEDOUT`,
-        `Unexpected socket close`,
-      ];
       networkErrors.forEach((errorName) => {
         it(`Node.js 네트워크 레벨의 ${errorName}에러가 발생하면 재시도한다.`, async () => {
           //given
@@ -72,15 +86,6 @@ describe('email consumer unit test', () => {
       });
 
       // SMTP 레벨의 400번대 에러 검증
-      const commonSmtp4xxErrors = [
-        {
-          responseCode: 421,
-          message: 'Service not available, closing transmission channel',
-        },
-        { responseCode: 450, message: 'Mailbox unavailable' },
-        { responseCode: 451, message: 'Local error in processing' },
-        { responseCode: 452, message: 'Insufficient system storage' },
-      ];
       commonSmtp4xxErrors.forEach(({ responseCode, message }) => {
         it(`SMTP ${responseCode} 에러가 발생하면 재시도한다.`, async () => {
           //given
@@ -119,70 +124,10 @@ describe('email consumer unit test', () => {
           );
         });
       });
-
-      const allErrors = [...networkErrors, ...commonSmtp4xxErrors];
-      allErrors.forEach((targetError) => {
-        const errorName =
-          typeof targetError === 'string'
-            ? targetError
-            : `SMTP ${targetError.responseCode}`;
-        it(`${errorName} 에러에 대해 재시도 횟수가 3회 이상이면 DLQ로 메시지를 발행한다.`, async () => {
-          //given
-          let error;
-          if (typeof targetError === 'string') {
-            error = new Error(`${targetError}`) as any;
-            if (errorName === 'ESOCKET') {
-              error.code = 'ESOCKET';
-            }
-          } else {
-            error = new Error(`${targetError.message}`) as any;
-            error.responseCode = targetError.responseCode;
-          }
-          const emailPayload: EmailPayload = {
-            type: EmailPayloadConstant.USER_CERTIFICATION,
-            data: {
-              email: `test@test.com`,
-              userName: `tester`,
-              uuid: `tester-uuid`,
-            },
-          };
-          const retryCount = 3;
-
-          //when
-          await emailConsumer.handleEmailByError(
-            error,
-            emailPayload,
-            retryCount,
-          );
-
-          //then
-          //1. 호출횟수 검증
-          expect(rabbitmqService.sendMessageToQueue).toHaveBeenCalledTimes(1);
-
-          //2. 입력 인자 검증
-          expect(rabbitmqService.sendMessageToQueue).toHaveBeenCalledWith(
-            RMQ_QUEUES.EMAIL_DEAD_LETTER,
-            JSON.stringify(emailPayload),
-            expect.objectContaining({
-              headers: expect.objectContaining({
-                'x-retry-count': retryCount,
-                'x-error-message': error.message,
-                'x-failure-type': 'MAX_RETRIES_EXCEEDED',
-              }),
-            }),
-          );
-        });
-      });
     });
 
     describe('영구적 실패, 즉시 DLQ로 발행되는 에러', () => {
       // SMTP 레벨의 500번대 에러
-      const commonSmtp5xxErrors = [
-        { responseCode: 550, message: 'Mailbox unavailable' },
-        { responseCode: 552, message: 'Exceeded storage allocation' },
-        { responseCode: 554, message: 'Transaction failed' },
-      ];
-
       commonSmtp5xxErrors.forEach(({ responseCode, message }) => {
         it(`SMTP ${responseCode} 에러가 발생하면 DLQ로 메시지를 발행한다.`, async () => {
           //given
@@ -257,6 +202,62 @@ describe('email consumer unit test', () => {
             }),
           }),
         );
+      });
+    });
+
+    describe('재시도 횟수 초과', () => {
+      const allErrors = [...networkErrors, ...commonSmtp4xxErrors];
+      allErrors.forEach((targetError) => {
+        const errorName =
+          typeof targetError === 'string'
+            ? targetError
+            : `SMTP ${targetError.responseCode}`;
+        it(`${errorName} 에러에 대해 재시도 횟수가 ${RETRY_CONFIG.MAX_RETRY}회 이상이면 DLQ로 메시지를 발행한다.`, async () => {
+          //given
+          let error;
+          if (typeof targetError === 'string') {
+            error = new Error(`${targetError}`) as any;
+            if (errorName === 'ESOCKET') {
+              error.code = 'ESOCKET';
+            }
+          } else {
+            error = new Error(`${targetError.message}`) as any;
+            error.responseCode = targetError.responseCode;
+          }
+          const emailPayload: EmailPayload = {
+            type: EmailPayloadConstant.USER_CERTIFICATION,
+            data: {
+              email: `test@test.com`,
+              userName: `tester`,
+              uuid: `tester-uuid`,
+            },
+          };
+          const retryCount = RETRY_CONFIG.MAX_RETRY;
+
+          //when
+          await emailConsumer.handleEmailByError(
+            error,
+            emailPayload,
+            retryCount,
+          );
+
+          //then
+          //1. 호출횟수 검증
+          expect(rabbitmqService.sendMessageToQueue).toHaveBeenCalledTimes(1);
+
+          //2. 입력 인자 검증
+          expect(rabbitmqService.sendMessageToQueue).toHaveBeenCalledWith(
+            RMQ_QUEUES.EMAIL_DEAD_LETTER,
+            JSON.stringify(emailPayload),
+            expect.objectContaining({
+              headers: expect.objectContaining({
+                'x-retry-count': retryCount,
+                'x-error-message': error.message,
+                'x-failure-type': 'MAX_RETRIES_EXCEEDED',
+              }),
+            }),
+          );
+        });
       });
     });
   });
