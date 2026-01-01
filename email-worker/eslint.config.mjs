2a9dae64864a721a3e2c2efafe83cf8dd70c0a4b
J_Coder <seok3765@naver.com>
2026-01-01 20:16:59 +0900

    📦 chore: email worker eslint 적용

--- email-worker/eslint.config.mjs
+++ email-worker/eslint.config.mjs
@@ -2,6 +2,11 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import js from '@eslint/js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules']),
@@ -10,10 +15,16 @@ export default defineConfig([
     extends: [
       eslintConfigPrettier,
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
     ],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
     },
   },
 ]);
