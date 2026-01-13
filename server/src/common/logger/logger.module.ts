1e6a83e9a898668da47c3127085eff9360da4b06
J_Coder <seok3765@naver.com>
2026-01-13 13:51:54 +0900

    ♻️ refactor: server 로그 출력 범위 수정

--- server/src/common/logger/logger.module.ts
+++ server/src/common/logger/logger.module.ts
@@ -1,24 +1,18 @@
 import { Global, Module } from '@nestjs/common';
 
-import * as winston from 'winston';
 import { WinstonModule } from 'nest-winston';
 
-import { getLogTransport, logFormat } from '@common/logger/logger.config';
+import { getLogTransport } from '@common/logger/logger.config';
 import { WinstonLoggerService } from '@common/logger/logger.service';
-
-const winstonModule = WinstonModule.forRoot({
-  // 로그 출력 형식에 대한 정의
-  format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    logFormat,
-  ),
-  transports: getLogTransport(),
-  silent: process.env.NODE_ENV === 'TEST',
-});
 
 @Global()
 @Module({
-  imports: [winstonModule],
+  imports: [
+    WinstonModule.forRoot({
+      transports: getLogTransport(),
+      silent: process.env.NODE_ENV === 'TEST',
+    }),
+  ],
   providers: [WinstonLoggerService],
   exports: [WinstonLoggerService],
 })
