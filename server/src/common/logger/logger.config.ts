a29f5f3dde80270cec708bf7f90253749740528f
J_Coder <seok3765@naver.com>
2026-01-13 13:51:54 +0900

    ♻️ refactor: server 로그 출력 범위 수정

--- server/src/common/logger/logger.config.ts
+++ server/src/common/logger/logger.config.ts
@@ -3,16 +3,26 @@ import DailyRotateFile from 'winston-daily-rotate-file';
 
 // 로그 출력 포맷 정의 함수
 export const logFormat = winston.format.printf(
-  ({ level, message, timestamp }) => {
-    return `${timestamp as string} ${level}: ${message as string}`;
-  },
+  ({ level, message, timestamp }) =>
+    `${timestamp as string} [${level}]: ${message as string}`,
 );
 
 const logDir = `${process.cwd()}/logs`;
 
 export function getLogTransport() {
   const transports = [];
-  transports.push(new winston.transports.Console());
+  const consoleLevel = process.env.NODE_ENV === 'DEV' ? 'silly' : 'info';
+
+  transports.push(
+    new winston.transports.Console({
+      level: consoleLevel,
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        logFormat,
+      ),
+    }),
+  );
 
   if (process.env.NODE_ENV === 'LOCAL' || process.env.NODE_ENV === 'PROD') {
     transports.push(
@@ -36,18 +46,6 @@ export function getLogTransport() {
           zippedArchive: true,
         }),
       ],
-    );
-  }
-
-  if (process.env.NODE_ENV === 'LOCAL' || process.env.NODE_ENV === 'DEV') {
-    transports.push(
-      new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-          logFormat,
-        ),
-      }),
     );
   }
 
