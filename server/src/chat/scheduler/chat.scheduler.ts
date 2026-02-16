64b96112ea5bcfcc7ff5e61404903aec137981cd
codeVac513 <xssdsw@naver.com>
2026-02-16 16:32:28 +0900

    ✨ feat: 자정 스케줄링 시 Redis의 status를 활용하도록 수정

--- server/src/chat/scheduler/chat.scheduler.ts
+++ server/src/chat/scheduler/chat.scheduler.ts
@@ -4,28 +4,36 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import type { BroadcastPayload } from '@chat/constant/chat.constant';
 import { ChatService } from '@chat/service/chat.service';
 
+import { WinstonLoggerService } from '@common/logger/logger.service';
+
 const CHAT_MIDNIGHT_CLIENT_NAME = 'system';
 
 @Injectable()
 export class ChatScheduler {
-  private dayInit: boolean = false;
-
-  constructor(private readonly chatService: ChatService) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly logger: WinstonLoggerService,
+  ) {}
 
   async handleDateMessage() {
-    if (this.dayInit) {
-      this.dayInit = false;
-      return await this.saveDateMessage();
+    const dateString = await this.chatService.getMidnightStatus();
+    if (!dateString) return;
+    const date = new Date(dateString);
+
+    if (!Number.isNaN(date.getTime())) {
+      return await this.saveDateMessage(date);
     }
+
+    this.logger.warn(`Invalid date format이 저장되었습니다: ${dateString}`);
   }
 
-  private async saveDateMessage() {
+  private async saveDateMessage(date: Date) {
     const broadcastPayload: BroadcastPayload = {
       userId: CHAT_MIDNIGHT_CLIENT_NAME,
       messageId: CHAT_MIDNIGHT_CLIENT_NAME,
       username: CHAT_MIDNIGHT_CLIENT_NAME,
       message: '',
-      timestamp: new Date(),
+      timestamp: date,
     };
 
     await this.chatService.saveMessageToRedis(broadcastPayload);
@@ -33,8 +41,10 @@ export class ChatScheduler {
     return broadcastPayload;
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-  private midnightInitializer() {
-    this.dayInit = true;
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
+    timeZone: 'Asia/Seoul',
+  })
+  private async midnightInitializer() {
+    await this.chatService.saveMidnightStatus();
   }
 }
