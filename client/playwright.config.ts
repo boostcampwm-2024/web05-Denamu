f7588206d3e36cb7a0a97a3d45664acd24311036
CodeVac513 <xssdsw@naver.com>
2026-02-26 15:36:35 +0900

    📦 chore: playwright env 설정 추가

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -1,12 +1,14 @@
+import { config as loadEnv } from "dotenv";
+
 import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+if (!process.env.CI) {
+  loadEnv({ path: ".env.e2e" });
+}
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +28,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.E2E_BASE_URL || "https://localhost",
+    ignoreHTTPSErrors: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
