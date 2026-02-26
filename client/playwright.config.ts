ea05ad847da24cf71ecc09ffb62df441d0d19026
CodeVac513 <xssdsw@naver.com>
2026-02-26 16:01:29 +0900

    📦 chore: dotenv 의존성 추가

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -6,9 +6,7 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-if (!process.env.CI) {
-  loadEnv({ path: ".env.e2e" });
-}
+loadEnv({ path: ".env.e2e" });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
