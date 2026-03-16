2716377ca432f6c97c2c4b7d4e4d4474f229d594
CodeVac513 <xssdsw@naver.com>
2026-03-16 15:43:29 +0900

    ✨ feat: discord를 활용한 알림 클래스 구현

---
+++ email-worker/src/notification/discord.notifier.ts
@@ -0,0 +1,51 @@
+import { injectable } from 'tsyringe';
+
+import axios from 'axios';
+import { EventEmitter } from 'node:events';
+
+import logger from '@src/logger';
+import { Notifier } from '@src/notification/notifier.interface';
+
+@injectable()
+export class DiscordNotifier implements Notifier {
+  private webhookUrl: string;
+  private webhook: string;
+  private eventListener: EventEmitter;
+  private started = false;
+
+  constructor() {
+    this.webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+    this.webhook = `DISCORD`;
+    if (!this.webhookUrl) {
+      throw new Error(`${this.webhook} Webhook url이 설정되지 않았습니다.`);
+    }
+    this.eventListener = new EventEmitter();
+  }
+
+  start() {
+    if (!this.started) {
+      this.eventListener.on('email.dlq', this.notify);
+      this.started = true;
+    }
+  }
+
+  notify = async (
+    payload: { error: Error; dlqMessage: string },
+    temp: DiscordNotifier,
+  ) => {
+    const { error, dlqMessage } = payload;
+    const discordStartTime = Date.now();
+    try {
+      await axios.post(this.webhookUrl, {
+        content: `${dlqMessage} DLQ 메시지 발행 - 오류 메시지: \`\`\`${error.message}\`\`\``,
+      });
+    } catch (e) {
+      logger.error('Discord 알림 전송 실패:', e);
+    }
+    logger.info(`알림 소요 시간: ${Date.now() - discordStartTime}`);
+  };
+
+  callEvent(eventName: string, payload: { error: Error; dlqMessage: string }) {
+    this.eventListener.emit(eventName, payload);
+  }
+}
