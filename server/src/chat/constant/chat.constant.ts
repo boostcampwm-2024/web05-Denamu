09a532ac302ec1c1e5fe9f39a756a6553e544fbd
CodeVac513 <xssdsw@naver.com>
2026-03-24 13:26:40 +0900

    ♻️ refactor: date message를 chat service에서 저장하기 위해서 scheduler의 코드 삭제 및 constant 추출

--- server/src/chat/constant/chat.constant.ts
+++ server/src/chat/constant/chat.constant.ts
@@ -1,6 +1,7 @@
 export const MAX_CLIENTS = 500;
 export const CLIENT_KEY_PREFIX = 'socket_client:';
 export const CHAT_HISTORY_LIMIT = 20;
+export const CHAT_MIDNIGHT_CLIENT_NAME = 'system';
 export type BroadcastPayload = {
   messageId: string;
   userId: string;
