6ba67c0637ea081a2b9320162dae8aefb47ba1ed
CodeVac513 <xssdsw@naver.com>
2026-03-24 13:30:52 +0900

    🧼 clean: 코드 정리

--- server/src/chat/scheduler/chat.scheduler.ts
+++ server/src/chat/scheduler/chat.scheduler.ts
@@ -1,12 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
-import type { BroadcastPayload } from '@chat/constant/chat.constant';
 import { ChatService } from '@chat/service/chat.service';
-
-import { WinstonLoggerService } from '@common/logger/logger.service';
-
-const CHAT_MIDNIGHT_CLIENT_NAME = 'system';
 
 @Injectable()
 export class ChatScheduler {
