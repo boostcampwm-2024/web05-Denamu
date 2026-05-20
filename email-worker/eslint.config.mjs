b27482086ef869bda98e747aa64e4f2bd478de39
J_Coder <seok3765@naver.com>
2026-05-20 22:37:20 +0900

    📦 chore: eslint 수정

--- email-worker/eslint.config.mjs
+++ email-worker/eslint.config.mjs
@@ -1,9 +1,9 @@
+import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-import js from '@eslint/js';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,7 +23,6 @@ export default defineConfig([
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
     },
     languageOptions: {
       parserOptions: {
