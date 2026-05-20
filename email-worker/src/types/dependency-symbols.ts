782922f3580684a3a3567e48941139c9c5543ed5
J_Coder <seok3765@naver.com>
2026-05-20 19:31:26 +0900

    🧼 clean: 문자열 하드코딩 -\> 클래스 meta 속성으로 변경

--- email-worker/src/types/dependency-symbols.ts
+++ email-worker/src/types/dependency-symbols.ts
@@ -1,7 +1,12 @@
+import { EmailConsumer } from '@src/email/email.consumer';
+import { EmailService } from '@src/email/email.service';
+import { RabbitMQManager } from '@src/rabbitmq/rabbitmq.manager';
+import { RabbitMQService } from '@src/rabbitmq/rabbitmq.service';
+
 export const DEPENDENCY_SYMBOLS = {
-  RabbitMQService: Symbol.for('RabbitMQService'),
-  RabbitMQManager: Symbol.for('RabbitMQManager'),
-  EmailConsumer: Symbol.for('EmailConsumer'),
-  EmailService: Symbol.for('EmailService'),
+  RabbitMQService: Symbol.for(RabbitMQService.name),
+  RabbitMQManager: Symbol.for(RabbitMQManager.name),
+  EmailConsumer: Symbol.for(EmailConsumer.name),
+  EmailService: Symbol.for(EmailService.name),
   Notifier: Symbol.for('Notifier'),
 };
