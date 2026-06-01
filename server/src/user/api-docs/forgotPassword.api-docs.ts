942e0d621d3fc66da0cc7d925d81e039c40c95cc
J_Coder <seok3765@naver.com>
2026-06-01 09:49:56 +0900

    📝 docs: user swagger 수정

--- server/src/user/api-docs/forgotPassword.api-docs.ts
+++ server/src/user/api-docs/forgotPassword.api-docs.ts
@@ -1,23 +1,13 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+
+import { ApiMessageResponse } from '@common/swagger/swagger.helper';
+import { ForgotPasswordRequestDto } from '@user/dto/request/forgotPassword.dto';
 
 export function ApiForgotPassword() {
   return applyDecorators(
-    ApiOperation({
-      summary: '비밀번호 변경 요청 API',
-    }),
-    ApiOkResponse({
-      description: 'Ok',
-      schema: {
-        properties: {
-          message: {
-            type: 'string',
-          },
-        },
-      },
-      example: {
-        message: '비밀번호 재설정 링크를 이메일로 발송했습니다.',
-      },
-    }),
+    ApiOperation({ summary: '비밀번호 변경 요청 API' }),
+    ApiBody({ type: ForgotPasswordRequestDto }),
+    ApiMessageResponse('비밀번호 재설정 이메일 발송 완료'),
   );
 }
--- server/src/user/api-docs/resetPassword.api-docs.ts
+++ server/src/user/api-docs/resetPassword.api-docs.ts
@@ -1,33 +1,17 @@
 import { applyDecorators } from '@nestjs/common';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+
 import {
-  ApiNotFoundResponse,
-  ApiOkResponse,
-  ApiOperation,
-} from '@nestjs/swagger';
+  ApiMessageResponse,
+  ApiNotFoundDoc,
+} from '@common/swagger/swagger.helper';
+import { ResetPasswordRequestDto } from '@user/dto/request/resetPassword.dto';
 
 export function ApiResetPassword() {
   return applyDecorators(
-    ApiOperation({
-      summary: '비밀번호 변경 API',
-    }),
-    ApiOkResponse({
-      description: 'Ok',
-      schema: {
-        properties: {
-          message: {
-            type: 'string',
-          },
-        },
-      },
-      example: {
-        message: '비밀번호가 성공적으로 수정되었습니다.',
-      },
-    }),
-    ApiNotFoundResponse({
-      description: 'Not Found',
-      example: {
-        message: '인증에 실패했습니다.',
-      },
-    }),
+    ApiOperation({ summary: '비밀번호 변경 API' }),
+    ApiBody({ type: ResetPasswordRequestDto }),
+    ApiMessageResponse('비밀번호 변경 완료'),
+    ApiNotFoundDoc('인증에 실패했습니다.'),
   );
 }
