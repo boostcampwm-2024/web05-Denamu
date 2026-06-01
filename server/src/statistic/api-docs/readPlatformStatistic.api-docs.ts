335b6bba635f7c91749f375297544cabf2b683c1
J_Coder <seok3765@naver.com>
2026-06-01 09:49:20 +0900

    📝 docs: statistic swagger 수정

--- server/src/statistic/api-docs/readPlatformStatistic.api-docs.ts
+++ server/src/statistic/api-docs/readPlatformStatistic.api-docs.ts
@@ -1,42 +1,12 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiOperation } from '@nestjs/swagger';
+
+import { ApiDataResponse } from '@common/swagger/swagger.helper';
+import { ReadStatisticPlatformResponseDto } from '@statistic/dto/response/readStatisticPlatform.dto';
 
 export function ApiReadPlatformStatistic() {
   return applyDecorators(
-    ApiOperation({
-      summary: '블로그 플랫폼 통계 조회 API',
-    }),
-    ApiOkResponse({
-      description: 'Ok',
-      schema: {
-        properties: {
-          message: {
-            type: 'string',
-          },
-          data: {
-            type: 'array',
-            items: {
-              properties: {
-                platform: {
-                  type: 'string',
-                },
-                count: {
-                  type: 'number',
-                },
-              },
-            },
-          },
-        },
-      },
-      example: {
-        message: '블로그 플랫폼 통계 조회 완료',
-        data: [
-          {
-            platform: 'test',
-            count: 30,
-          },
-        ],
-      },
-    }),
+    ApiOperation({ summary: '블로그 플랫폼 통계 조회 API' }),
+    ApiDataResponse(ReadStatisticPlatformResponseDto, true, '블로그 플랫폼 통계 조회 성공'),
   );
 }
