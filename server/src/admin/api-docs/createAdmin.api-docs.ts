922fdd3bcbb7f299a1aad44a06838d7bf4a99020
J_Coder <seok3765@naver.com>
2026-06-01 09:43:33 +0900

    📝 docs: admin swagger 수정

--- server/src/admin/api-docs/createAdmin.api-docs.ts
+++ server/src/admin/api-docs/createAdmin.api-docs.ts
@@ -1,47 +1,22 @@
 import { applyDecorators } from '@nestjs/common';
+import { ApiBody, ApiCookieAuth, ApiOperation } from '@nestjs/swagger';
+
 import {
-  ApiBadRequestResponse,
-  ApiConflictResponse,
-  ApiCreatedResponse,
-  ApiOperation,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
+  ApiBadRequestDoc,
+  ApiConflictDoc,
+  ApiCreatedDoc,
+  ApiUnauthorizedDoc,
+} from '@common/swagger/swagger.helper';
+import { RegisterAdminRequestDto } from '@admin/dto/request/registerAdmin.dto';
 
 export function ApiCreateAdmin() {
   return applyDecorators(
-    ApiOperation({
-      summary: `관리자 회원 가입 API`,
-    }),
-    ApiCreatedResponse({
-      description: 'Created',
-      schema: {
-        properties: {
-          message: {
-            type: 'string',
-          },
-        },
-      },
-      example: {
-        message: '성공적으로 관리자 계정이 생성되었습니다.',
-      },
-    }),
-    ApiBadRequestResponse({
-      description: 'Bad Request',
-      example: {
-        message: '오류 메세지',
-      },
-    }),
-    ApiConflictResponse({
-      description: 'Conflict',
-      example: {
-        message: '이미 존재하는 계정입니다.',
-      },
-    }),
-    ApiUnauthorizedResponse({
-      description: 'Unauthorized',
-      example: {
-        message: '인증되지 않은 요청입니다.',
-      },
-    }),
+    ApiCookieAuth('sessionId'),
+    ApiOperation({ summary: '관리자 회원 가입 API' }),
+    ApiBody({ type: RegisterAdminRequestDto }),
+    ApiCreatedDoc('관리자 계정 생성 성공'),
+    ApiBadRequestDoc('요청 데이터 검증에 실패했습니다.'),
+    ApiConflictDoc('이미 존재하는 계정입니다.'),
+    ApiUnauthorizedDoc('인증되지 않은 요청입니다.'),
   );
 }
