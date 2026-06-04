677baf2629d5b2d82b9ae81bd19aad7350cad26f
J_Coder <seok3765@naver.com>
2026-06-04 18:10:49 +0900

    📦 chore: vitest exclude 추가

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./src/__tests__/setup.tsx"],
     globals: true,
+    exclude: ["**/node_modules/**", "**/e2e/**"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
