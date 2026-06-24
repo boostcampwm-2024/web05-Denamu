aed30676579d3f0c2c490a3b53fb442b91ed5ec6
Min:D <seok3765@naver.com>
2026-06-24 16:00:43 +0900

    ♻️ refactor: params optional 처리

--- feed-crawler/src/common/database-connection.ts
+++
@@ -1,5 +0,0 @@
-export interface DatabaseConnection {
-  executeQuery<T>(query: string, params: any[]): Promise<T[] | null>;
-  executeQueryStrict<T>(query: string, params: any[]): Promise<T[]>;
-  end(): Promise<void>;
-}
