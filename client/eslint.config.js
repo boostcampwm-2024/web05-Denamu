d5fba45f0fe53b8395c90d414168870c0ed2d627
Min:D <seok3765@naver.com>
2026-06-25 21:45:30 +0900

    📦 chore: eslint ignore 경로 추가

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,13 +1,12 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import pluginReact from "eslint-plugin-react";
 import storybook from "eslint-plugin-storybook";
-
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist", "node_modules", "coverage"] },
+  { ignores: ["dist", "node_modules", "coverage", "storybook-static"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
@@ -17,5 +16,5 @@ export default [
       "react/react-in-jsx-scope": "off",
     },
   },
-  ...storybook.configs["flat/recommended"]
+  ...storybook.configs["flat/recommended"],
 ];
