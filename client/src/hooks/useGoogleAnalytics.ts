46f2ad66291514b88a496873e1a4b66f604e0386
Min:D <seok3765@naver.com>
2026-07-21 01:33:13 +0900

    ✨ feat: google Analytics 수집 기능 추가 (페이지별 기능도 추가)

---
+++ client/src/hooks/useGoogleAnalytics.ts
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import { trackPageView } from "@/utils/analytics";
+
+export const useGoogleAnalytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location.pathname, location.search]);
+};
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { setupMocks } from "@/api/mocks";
+import { initGA } from "@/utils/analytics";
 
 if (import.meta.env.DEV) {
   setupMocks();
 }
+
+initGA();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
