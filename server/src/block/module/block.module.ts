404bbbea0dfe5300d403ad51a88cc894c51c65c8
Min:D <seok3765@naver.com>
2026-07-24 00:16:13 +0900

    ✨ feat: rss block 서비스들 block 모듈에 추가

--- server/src/block/module/block.module.ts
+++ server/src/block/module/block.module.ts
@@ -4,14 +4,22 @@ import { JwtAuthModule } from '@common/auth/jwt.module';
 
 import { BlockController } from '@block/controller/block.controller';
 import { BlockRepository } from '@block/repository/block.repository';
+import { RssBlockRepository } from '@block/repository/rssBlock.repository';
 import { BlockService } from '@block/service/block.service';
+
+import { RssAcceptRepository } from '@rss/repository/rss.repository';
 
 import { UserModule } from '@user/module/user.module';
 
 @Module({
   imports: [JwtAuthModule, UserModule],
   controllers: [BlockController],
-  providers: [BlockService, BlockRepository],
+  providers: [
+    BlockService,
+    BlockRepository,
+    RssBlockRepository,
+    RssAcceptRepository,
+  ],
   exports: [],
 })
 export class BlockModule {}
