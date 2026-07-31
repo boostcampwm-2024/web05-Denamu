319a1607048e5ac60cbbe92f57b1788036ffa4a3
Min:D <seok3765@naver.com>
2026-08-01 06:57:56 +0900

    🐛 fix: vscode 저장 오류로 인한 모듈 imports 추가

--- server/src/common/throttler/login-throttler.module.ts
+++ server/src/common/throttler/login-throttler.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { ThrottlerModule } from '@nestjs/throttler';
 
 import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
-import Redis from 'ioredis';
+
+import { RedisModule } from '@common/redis/redis.module';
+import { RedisService } from '@common/redis/redis.service';
 
 @Module({
   imports: [
     ThrottlerModule.forRootAsync({
-      inject: ['REDIS_CLIENT'],
-      useFactory: (redisClient: Redis) => ({
+      imports: [RedisModule],
+      inject: [RedisService],
+      useFactory: (redisService: RedisService) => ({
         throttlers: [{ ttl: 60_000, limit: 5 }],
-        storage: new ThrottlerStorageRedisService(redisClient),
+        storage: new ThrottlerStorageRedisService(redisService.redisClient),
         errorMessage: '로그인 시도가 너무 많습니다. 잠시 후 다시 시도해주세요.',
       }),
     }),
