71606aaa73615d3aaa3fb8598b49e9f8d07d650d
Min:D <seok3765@naver.com>
2026-08-03 22:18:52 +0900

    💄 style: 하이퍼링크 색상 변경

--- client/src/components/board/BoardContent.tsx
+++ client/src/components/board/BoardContent.tsx
@@ -7,7 +7,7 @@ interface BoardContentProps {
 export const BoardContent = ({ content }: BoardContentProps) => {
   return (
     <div
-      className="prose max-w-full prose-p:my-2 [&_p:empty]:min-h-[1.75em]"
+      className="prose max-w-full prose-p:my-2 prose-a:text-primary prose-a:no-underline hover:prose-a:underline [&_p:empty]:min-h-[1.75em]"
       dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
     />
   );
