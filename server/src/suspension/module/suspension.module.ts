bab83e74a74e4c6daf1881518ae309b4759199cf
Min:D <seok3765@naver.com>
2026-08-10 21:14:48 +0900

    ✨ feat: rss 정지 레포지토리 추가

--- server/src/suspension/module/suspension.module.ts
+++ server/src/suspension/module/suspension.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AdminRepository } from '@admin/repository/admin.repository';
 
 import { AdminSuspensionController } from '@suspension/controller/adminSuspension.controller';
+import { RssSuspensionRepository } from '@suspension/repository/rssSuspension.repository';
 import { UserSuspensionRepository } from '@suspension/repository/userSuspension.repository';
 import { SuspensionService } from '@suspension/service/suspension.service';
 
@@ -11,7 +12,12 @@ import { UserModule } from '@user/module/user.module';
 @Module({
   imports: [UserModule],
   controllers: [AdminSuspensionController],
-  providers: [SuspensionService, UserSuspensionRepository, AdminRepository],
+  providers: [
+    SuspensionService,
+    UserSuspensionRepository,
+    RssSuspensionRepository,
+    AdminRepository,
+  ],
   exports: [SuspensionService],
 })
 export class SuspensionModule {}
